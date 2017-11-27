29d00a68a91a681e8e2dc7e816934b3ca2a467e0
Davide Bettio <davide@uninstall.it>
2017-11-28 00:49:34 +0100

    Add GlobalContext
    GlobalContext stores context shared among all processes, like scheduling
    queues.

---
+++ src/libAtomVM/globalcontext.c
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *   Copyright 2017 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "globalcontext.h"
+
+GlobalContext *globalcontext_new()
+{
+    GlobalContext *glb = malloc(sizeof(GlobalContext));
+    glb->ready_processes = NULL;
+    glb->waiting_processes = NULL;
+    glb->listeners = NULL;
+
+    return glb;
+}
---
+++ src/libAtomVM/globalcontext.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright 2017 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef _GLOBALCONTEXT_H_
+#define _GLOBALCONTEXT_H_
+
+#include "linkedlist.h"
+
+struct GlobalContext;
+
+typedef struct
+{
+    struct ListHead *ready_processes;
+    struct ListHead *waiting_processes;
+    struct ListHead *listeners;
+} GlobalContext;
+
+extern GlobalContext *globalcontext_new();
+
+#endif
