245d525f98ab168b8033d138bbca439a6d7aa310
Davide Bettio <davide@uninstall.it>
2017-11-30 21:18:05 +0100

    Add mailbox.c
    mailbox implements messages queue manipulation functions.

---
+++ src/libAtomVM/mailbox.c
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright 2017 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "mailbox.h"
+
+void mailbox_send(Context *c, term t)
+{
+    Message *m = malloc(sizeof(Message));
+    m->message = t;
+    linkedlist_append(&c->mailbox, &m->mailbox_list_head);
+}
+
+term mailbox_receive(Context *c)
+{
+    Message *m = LIST_ENTRY(c->mailbox, Message, mailbox_list_head);
+    linkedlist_remove(&c->mailbox, &m->mailbox_list_head);
+
+    term rt = m->message;
+
+    free(m);
+
+    return rt;
+}
---
+++ src/libAtomVM/mailbox.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright 2017 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef _MAILBOX_H_
+#define _MAILBOX_H_
+
+#include "linkedlist.h"
+#include "Term.h"
+#include "Context.h"
+
+typedef struct
+{
+    struct ListHead mailbox_list_head;
+    term message;
+} Message;
+
+extern void mailbox_send(Context *c, term t);
+extern term mailbox_receive(Context *c);
+
+#endif
