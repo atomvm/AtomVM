2e9348e9dd757a956f9f0cd6d065d234f7bcab43
Davide Bettio <davide@uninstall.it>
2018-01-02 21:01:15 +0100

    tests: Add registered echo port driver test
    Create a new port, register it and send values.

---
+++ tests/erlang_tests/test_regecho_driver.erl
@@ -0,0 +1,16 @@
+-module (test_regecho_driver).
+-export([start/0, do_open_port/2, echo/1]).
+
+start() ->
+    register(echo, do_open_port("echo", nil)),
+    length(echo("Hello World")).
+
+do_open_port(PortName, Param) ->
+    open_port({spawn, PortName}, Param).
+
+echo(SendValue) ->
+    whereis(echo) ! {self(), SendValue},
+    receive
+        Value ->
+            Value
+    end.
