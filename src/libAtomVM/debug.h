4c0385f325688b06bc9a11d7a3ddeceffaf126cd
Davide Bettio <davide@uninstall.it>
2018-01-15 19:23:21 +0100

    debug.h add doxygen documentation
    Add doxgen documentation to debug functions and header.

--- src/libAtomVM/debug.h
+++ src/libAtomVM/debug.h
@@ -17,12 +17,33 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
  ***************************************************************************/
 
+/**
+ * @file debug.h
+ * @brief Debug functions and macros.
+ *
+ * @details Miscellaneous functions and macros useful for debug.
+ */
+
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
 #include "context.h"
 
+/**
+ * @brief Print stack content to stderr.
+ *
+ * @details Print the dump of the stack of the given context to stderr.
+ * @param ctx the process context.
+ */
 extern void debug_dump_stack(Context *ctx);
+
+/**
+ * @brief Gets a printable char for a given register type.
+ *
+ * @details Returns a printable char such as x or y for the given register type.
+ * @param reg_type register type.
+ * @return printable register type.
+ */
 extern char reg_type_c(int reg_type);
 
 #ifdef ENABLE_STACK_TRACE
