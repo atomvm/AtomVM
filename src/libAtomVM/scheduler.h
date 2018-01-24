8d89788f7ce07a399ebcb2f3ba25ff5d8be26e11
Davide Bettio <davide@uninstall.it>
2018-01-24 20:44:16 +0100

    scheduler.h: Add doxygen documentation

--- src/libAtomVM/scheduler.h
+++ src/libAtomVM/scheduler.h
@@ -17,6 +17,13 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
  ***************************************************************************/
 
+/**
+ * @file scheduler.h
+ * @brief Scheduling functions.
+ *
+ * @details Scheduling functions are used to schedule processes.
+ */
+
 #ifndef _SCHEDULER_H_
 #define _SCHEDULER_H_
 
@@ -26,16 +33,30 @@
 
 /**
  * @brief move a process to waiting queue and wait a ready one
+ *
+ * @details move current process to the waiting queue, and schedule the next one or sleep until an event is received.
+ * @param global the global context.
+ * @param c the process context.
+ * @param timeout in milliseconds.
  */
 extern Context *scheduler_wait(GlobalContext *global, Context *c, int timeout);
 
-
 /**
  * @brief make sure a process is on the ready queue
+ *
+ * @details make a process ready again by moving it to the ready queue.
+ * @param global the global context.
+ * @param c the process context.
  */
 extern void scheduler_make_ready(GlobalContext *global, Context *c);
 
-
+/**
+ * @brief just move a process to the wait queue
+ *
+ * @details make a process waiting.
+ * @param global the global context.
+ * @param c the process context.
+ */
 extern void scheduler_make_waiting(GlobalContext *global, Context *c);
 
 #endif
