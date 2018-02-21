645573d49302d4f585e5fca140c496a4ca800544
Davide Bettio <davide@uninstall.it>
2018-02-21 01:53:37 +0100

    Add Elixir Hello World
    Add HelloWorld.ex example.

---
+++ examples/elixir/HelloWorld.ex
@@ -0,0 +1,21 @@
+defmodule HelloWorld do
+
+  def start() do
+    console = do_open_port('console', [])
+    write(console, 'Hello World\n')
+  end
+
+  defp do_open_port(port_name, param) do
+    Port.open({:spawn, port_name}, param)
+  end
+
+  defp write(console, string) do
+    send console, {self(), string}
+
+    receive do
+      return_status ->
+        return_status
+    end
+  end
+
+end
