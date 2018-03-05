2c98201e031e6e57b28124517e110e7de52d6f25
Davide Bettio <davide@uninstall.it>
2018-03-05 14:27:17 +0100

    packbeam: use new avm pack file format
    Add support to multiple modules, mark the startup module and allow to
    execute it using #! magic.

--- tools/packbeam/packbeam.c
+++ tools/packbeam/packbeam.c
@@ -18,6 +18,7 @@
  ***************************************************************************/
 
 #include <assert.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,8 +31,13 @@
 #define LITT_UNCOMPRESSED_SIZE_OFFSET 8
 #define LITT_HEADER_SIZE 12
 
+#define END_OF_FILE 0
+#define BEAM_START_FLAG 1
+#define BEAM_CODE_FLAG 2
+
 static void pad_and_align(FILE *f);
 static void *uncompress_literals(const uint8_t *litT, int size, size_t *uncompressedSize);
+static void add_module_header(FILE *f, const char *module_name, uint32_t flags);
 
 int main(int argc, char **argv)
 {
@@ -41,13 +47,16 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    unsigned char beam_header[12] =
+    const unsigned char pack_header[24] =
     {
-        0x46, 0x4f, 0x52, 0x31,
-        0x00, 0x00, 0x00, 0x00,
-        0x42, 0x45, 0x41, 0x4d
+        0x23, 0x21, 0x2f, 0x75,
+        0x73, 0x72, 0x2f, 0x62,
+        0x69, 0x6e, 0x2f, 0x65,
+        0x6e, 0x76, 0x20, 0x41,
+        0x74, 0x6f, 0x6d, 0x56,
+        0x4d, 0x0a, 0x00, 0x00
     };
-    assert(fwrite(beam_header, 1, 12, pack) == 12);
+    assert(fwrite(pack_header, sizeof(unsigned char), 24, pack) == 24);
 
     for (int i = 2; i < argc; i++) {
         FILE *beam = fopen(argv[i], "r");
@@ -57,15 +66,34 @@ int main(int argc, char **argv)
         }
 
         fseek(beam, 0, SEEK_END);
-        size_t size = ftell(beam);
+        size_t beam_size = ftell(beam);
         fseek(beam, 0, SEEK_SET);
+        size_t zero_pos = ftell(pack);
 
-        uint8_t *data = malloc(size);
-        assert(fread(data, sizeof(uint8_t), size, beam) == size);
+        char *complete_name = strdup(argv[i]);
+        char *filename = basename(complete_name);
+        if (i == 2) {
+            add_module_header(pack, filename, BEAM_CODE_FLAG | BEAM_START_FLAG);
+        } else {
+            add_module_header(pack, filename, BEAM_CODE_FLAG);
+        }
+        free(complete_name);
+
+        int written_beam_header_pos = ftell(pack);
+        const unsigned char beam_header[12] =
+        {
+            0x46, 0x4f, 0x52, 0x31,
+            0x00, 0x00, 0x00, 0x00,
+            0x42, 0x45, 0x41, 0x4d
+        };
+        assert(fwrite(beam_header, 1, 12, pack) == 12);
+
+        uint8_t *data = malloc(beam_size);
+        assert(fread(data, sizeof(uint8_t), beam_size, beam) == beam_size);
 
         unsigned long offsets[MAX_OFFS];
         unsigned long sizes[MAX_SIZES];
-        scan_iff(data, size, offsets, sizes);
+        scan_iff(data, beam_size, offsets, sizes);
 
         if (offsets[AT8U]) {
             assert(fwrite(data + offsets[AT8U], sizeof(uint8_t), sizes[AT8U] + IFF_SECTION_HEADER_SIZE, pack) == sizes[AT8U] + IFF_SECTION_HEADER_SIZE);
@@ -97,13 +125,23 @@ int main(int argc, char **argv)
             assert(fwrite(deflated, sizeof(uint8_t), u_size, pack) == u_size);
             free(deflated);
         }
+
+        size_t end_of_module_pos = ftell(pack);
+
+        size_t size = end_of_module_pos - zero_pos;
+        uint32_t size_field = ENDIAN_SWAP_32(size);
+        fseek(pack, zero_pos, SEEK_SET);
+        assert(fwrite(&size_field, sizeof(uint32_t), 1, pack) == 1);
+        fseek(pack, end_of_module_pos, SEEK_SET);
+
+        int beam_written_size = end_of_module_pos - written_beam_header_pos;
+        uint32_t beam_written_size_field = ENDIAN_SWAP_32(beam_written_size);
+        fseek(pack, written_beam_header_pos + 4, SEEK_SET);
+        assert(fwrite(&beam_written_size_field , sizeof(uint32_t), 1, pack) == 1);
+        fseek(pack, end_of_module_pos, SEEK_SET);
     }
 
-    fseek(pack, 0, SEEK_END);
-    size_t size = ftell(pack);
-    uint32_t size_field = ENDIAN_SWAP_32(size);
-    fseek(pack, 4, SEEK_SET);
-    assert(fwrite(&size_field, sizeof(uint32_t), 1, pack) == 1);
+    add_module_header(pack, "end", END_OF_FILE);
 
     return EXIT_SUCCESS;
 }
@@ -146,6 +184,24 @@ static void *uncompress_literals(const uint8_t *litT, int size, size_t *uncompre
 static void pad_and_align(FILE *f)
 {
     while ((ftell(f) % 4) != 0) {
+        fputc(0, f);
+    }
+}
+
+static void add_module_header(FILE *f, const char *module_name, uint32_t flags)
+{
+    int module_name_size = strlen(module_name) + 1;
+    int padded_module_name_size = ((module_name_size + 4 - 1) >> 2) << 2;
+
+    uint32_t size_field = 0;
+    uint32_t flags_field = ENDIAN_SWAP_32(flags);
+    uint32_t reserved = 0;
+
+    assert(fwrite(&size_field, sizeof(uint32_t), 1, f) == 1);
+    assert(fwrite(&flags_field, sizeof(uint32_t), 1, f) == 1);
+    assert(fwrite(&reserved, sizeof(uint32_t), 1, f) == 1);
+    assert(fwrite(module_name, sizeof(char), strlen(module_name) + 1, f) == strlen(module_name) + 1);
+    for (int i = 0; i < padded_module_name_size - module_name_size; i++) {
         fputc(0, f);
     }
 }
