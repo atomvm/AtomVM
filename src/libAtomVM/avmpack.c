1323d92604b34c8729d7992339333ff98f7ff2a2
Davide Bettio <davide@uninstall.it>
2018-03-06 00:26:58 +0100

    Add AVMPack format parser
    Add parser for AtomVM Pack files, that they pack together several BEAM
    modules in one file.

---
+++ src/libAtomVM/avmpack.c
@@ -0,0 +1,102 @@
+/***************************************************************************
+ *   Copyright 2018 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "avmpack.h"
+#include "utils.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdio.h>
+
+#define AVMPACK_SIZE 24
+
+static inline int pad(int size)
+{
+    return ((size + 4 - 1) >> 2) << 2;
+}
+
+int avmpack_is_valid(const void *avmpack_binary, uint32_t size)
+{
+    const unsigned char pack_header[AVMPACK_SIZE] =
+    {
+        0x23, 0x21, 0x2f, 0x75,
+        0x73, 0x72, 0x2f, 0x62,
+        0x69, 0x6e, 0x2f, 0x65,
+        0x6e, 0x76, 0x20, 0x41,
+        0x74, 0x6f, 0x6d, 0x56,
+        0x4d, 0x0a, 0x00, 0x00
+    };
+
+    if (size < 24) {
+        return 0;
+    }
+
+    return memcmp(avmpack_binary, pack_header, AVMPACK_SIZE) == 0;
+}
+
+int avmpack_find_section_by_flag(const void *avmpack_binary, int flags_mask, const void **ptr, uint32_t *size)
+{
+    int offset = AVMPACK_SIZE;
+    const uint32_t *flags;
+
+    do {
+        const uint32_t *sizes = ((const uint32_t *) (avmpack_binary)) + offset/sizeof(uint32_t);
+        flags = ((const uint32_t *) (avmpack_binary)) + 1 + offset/sizeof(uint32_t);
+
+        if ((ENDIAN_SWAP_32(*flags) & flags_mask) == flags_mask) {
+            const char *found_section_name = (const char *) (sizes + 3);
+            int section_name_len = pad(strlen(found_section_name));
+
+            *ptr = sizes + 3 + section_name_len/sizeof(uint32_t);
+            *size = ENDIAN_SWAP_32(*sizes);
+            return 1;
+        }
+
+        offset += ENDIAN_SWAP_32(*sizes);
+
+    } while(*flags);
+
+    return 0;
+}
+
+int avmpack_find_section_by_name(const void *avmpack_binary, const char *name, const void **ptr, uint32_t *size)
+{
+    int offset = AVMPACK_SIZE;
+    const uint32_t *flags;
+
+    do {
+        const uint32_t *sizes = ((const uint32_t *) (avmpack_binary)) + offset/sizeof(uint32_t);
+        flags = ((const uint32_t *) (avmpack_binary)) + 1 + offset/sizeof(uint32_t);
+
+        const char *found_section_name = (const char *) (sizes + 3);
+        if (!strcmp(name, found_section_name)) {
+            int section_name_len = pad(strlen(found_section_name));
+
+            *ptr = sizes + 3 + section_name_len/sizeof(uint32_t);
+            *size = ENDIAN_SWAP_32(*sizes);
+            return 1;
+        }
+
+        offset += ENDIAN_SWAP_32(*sizes);
+
+    } while(*flags);
+
+    return 0;
+}
---
+++ src/libAtomVM/avmpack.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright 2018 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+/**
+ * @file avmpack.h
+ * @brief AVM Pack file format parsing function.
+ */
+
+#ifndef _AVMPACK_H_
+#define _AVMPACK_H_
+
+#include <stdint.h>
+
+#define END_OF_FILE 0
+#define BEAM_START_FLAG 1
+#define BEAM_CODE_FLAG 2
+
+/**
+ * @brief Finds an AVM Pack section that has certain flags set.
+ *
+ * @details Finds an AVM Pack section that has certain flags set and returns a pointer to it and its size.
+ * @param avmpack_binary a pointer to valid AVM Pack file data.
+ * @param flags_mask that will be matched against file sections.
+ * @param ptr will point to the found file section.
+ * @param size will be set to the file section size that has been found, if the section has not been found it will not be updated.
+ * @returns 1 if the file section has been found, 0 otherwise.
+ */
+int avmpack_find_section_by_flag(const void *avmpack_binary, int flags_mask, const void **ptr, uint32_t *size);
+
+/**
+ * @brief Finds an AVM Pack section that has certain name.
+ *
+ * @details Finds an AVM Pack section with a certain name and returns a pointer to it and its size.
+ * @param avmpack_binary a pointer to valid AVM Pack file data.
+ * @param name the file section name that will be searched.
+ * @param ptr will point to the found file section, if the section has not been found it will not be updated.
+ * @param size will be set to the file section size that has been found, if the section has not been found it will not be updated.
+ * @returns 1 if the file section has been found, 0 otherwise.
+ */
+
+int avmpack_find_section_by_name(const void *avmpack_binary, const char *name, const void **ptr, uint32_t *size);
+
+/**
+ * @brief Returns 1 if the pointed binary is a valid AVM Pack.
+ *
+ * @details Returns if the pointed binary is a valid AVM Pack binary or not.
+ * @param avmpack_binary a pointer to an AVM Pack binary.
+ * @param size the size of AVM Pack binary.
+ * @returns 1 if it is a valid AVM Pack binary, 0 otherwise.
+ */
+int avmpack_is_valid(const void *avmpack_binary, uint32_t size);
+
+#endif
