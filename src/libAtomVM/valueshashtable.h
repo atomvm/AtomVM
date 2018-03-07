b303efbda8de3fc48fa4d98bb8cd67602f451ae5
Davide Bettio <davide@uninstall.it>
2018-03-07 21:34:25 +0100

    valueshashtable.h: fix typo
    Fix typo in include guard.

--- src/libAtomVM/valueshashtable.h
+++ src/libAtomVM/valueshashtable.h
@@ -17,7 +17,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
  ***************************************************************************/
 
-#ifndef _VALUESSHASHTABLE_H_
+#ifndef _VALUESHASHTABLE_H_
 #define _VALUESHASHTABLE_H_
 
 struct ValuesHashTable
