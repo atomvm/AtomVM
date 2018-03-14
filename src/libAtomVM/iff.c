4444d0d71cd31a664bc80935a481fd8541d50b29
Davide Bettio <davide@uninstall.it>
2018-03-14 01:46:24 +0100

    iff: print a warning in case of size mismatch
    Print a warning if given IFF buffer size is less than IFF size field.

--- src/libAtomVM/iff.c
+++ src/libAtomVM/iff.c
@@ -20,6 +20,7 @@
 #include "iff.h"
 #include "utils.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -50,6 +51,9 @@ void scan_iff(const void *iff_binary, int buf_size, unsigned long *offsets, unsi
 
     uint32_t iff_size = READ_32_ALIGNED(data + 4);
     int file_size = iff_size;
+    if (buf_size < file_size) {
+        fprintf(stderr, "warning: buffer holding IFF is smaller than IFF size: %i", buf_size);
+    }
 
     do {
         struct IFFRecord *current_record = (struct IFFRecord *) (data + current_pos);
