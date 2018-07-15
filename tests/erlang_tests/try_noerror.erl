f2479be0d6201a0bca47b8527a6e26859d187889
Davide Bettio <davide@uninstall.it>
2018-07-15 14:39:39 +0200

    tests: Add try_noerror test
    try_noerror doesn't handle any error, it just allows try, try_end and
    try_case decode testing.

---
+++ tests/erlang_tests/try_noerror.erl
@@ -0,0 +1,12 @@
+-module (try_noerror).
+-export([start/0, f/1]).
+
+start() ->
+    f(3).
+
+f(Value) ->
+    try 5 div Value of
+        AnyVal -> AnyVal
+    catch
+        _:_ -> error
+    end.
