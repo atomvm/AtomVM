afb9999f037b3adfbcf6e00ab46cd210bc54c9dd
Davide Bettio <davide@uninstall.it>
2018-07-29 18:51:46 +0200

    tests: add make_ref_test
    Add test for erlang:make_ref\0.

---
+++ tests/erlang_tests/make_ref_test.erl
@@ -0,0 +1,20 @@
+-module (make_ref_test).
+-export([start/0, compare/2, do_make_ref/0, to_tuple/1]).
+
+start() ->
+    ARef = do_make_ref(),
+    ARefT = to_tuple(ARef),
+    compare(ARefT, ARef) + compare(to_tuple(make_ref()), ARef).
+
+do_make_ref() ->
+    make_ref().
+
+to_tuple(Something) ->
+    {Something}.
+
+compare(Something1, {Something2}) when Something1 == Something2 ->
+    1;
+compare({Something1}, Something2) when Something1 == Something2 ->
+    2;
+compare(_Any1, _Any2) ->
+    3.
