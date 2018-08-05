c19af495007a11f94ddce68487ef8b74ca832f99
Davide Bettio <davide@uninstall.it>
2018-08-05 21:23:14 +0200

    opcodesswitch.h: add is_reference/1 instruction
    Add is_reference/1 test insturction that checks if a term is a
    reference.

--- src/libAtomVM/opcodesswitch.h
+++ src/libAtomVM/opcodesswitch.h
@@ -345,6 +345,7 @@
 #define OP_IS_NUMBER 47
 #define OP_IS_ATOM 48
 #define OP_IS_PID 49
+#define OP_IS_REFERENCE 50
 #define OP_IS_PORT 51
 #define OP_IS_NIL 52
 #define OP_IS_BINARY 53
@@ -1566,7 +1567,33 @@ static int get_catch_label_and_change_module(Context *ctx, Module **mod)
                 #endif
 
                 break;
-           }
+            }
+
+            case OP_IS_REFERENCE: {
+                int next_off = 1;
+                int label;
+                DECODE_LABEL(label, code, i, next_off, next_off)
+                term arg1;
+                DECODE_COMPACT_TERM(arg1, code, i, next_off, next_off)
+
+                #ifdef IMPL_EXECUTE_LOOP
+                    TRACE("is_reference/2, label=%i, arg1=%lx\n", label, arg1);
+
+                    if (term_is_reference(arg1)) {
+                        NEXT_INSTRUCTION(next_off);
+                    } else {
+                        i = POINTER_TO_II(mod->labels[label]);
+                    }
+                #endif
+
+                #ifdef IMPL_CODE_LOADER
+                    TRACE("is_reference/2\n");
+                    UNUSED(arg1)
+                    NEXT_INSTRUCTION(next_off);
+                #endif
+
+                break;
+            }
 
             case OP_IS_PORT: {
                 int next_off = 1;
