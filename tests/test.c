3c1d62697e15939f04920c0b43d342dfc2a924f1
Davide Bettio <davide@uninstall.it>
2018-10-21 11:40:41 +0200

    tests: add test_tl.erl
    Add test for erlang:tl/1 function.

--- tests/test.c
+++ tests/test.c
@@ -120,6 +120,7 @@ struct Test tests[] =
     {"test_delete_element.beam", 421},
     {"test_tuple_to_list.beam", 300},
     {"test_make_tuple.beam", 4},
+    {"test_tl.beam", 5},
 
     {"nested_list_size0.beam", 0},
     {"nested_list_size1.beam", 2},
