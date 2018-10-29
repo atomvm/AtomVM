44943e43e265848eecd8fb5e01c445f2722fb72d
Davide Bettio <davide@uninstall.it>
2018-10-30 00:47:08 +0100

    term: add term_to_ref_ticks function
    Add function that returns ref ticks from a ref term.

--- src/libAtomVM/term.h
+++ src/libAtomVM/term.h
@@ -536,6 +536,27 @@ static inline term term_from_ref_ticks(uint64_t ref_ticks, Context *ctx)
     return ((term) boxed_value) | TERM_BOXED_VALUE_TAG;
 }
 
+static inline uint64_t term_to_ref_ticks(term rt)
+{
+    const term *boxed_value = term_to_const_term_ptr(rt);
+    if ((boxed_value[0] & 0x3F) == TERM_BOXED_REF) {
+        int ref_size = (sizeof(uint64_t) / sizeof(term));
+
+        if (ref_size == 1) {
+            return boxed_value[1];
+
+        } else if (ref_size == 2) {
+            return (boxed_value[1] << 4) | boxed_value[2];
+
+        } else {
+            abort();
+        }
+
+    } else {
+        abort();
+    }
+}
+
 /**
  * @brief Allocates a tuple on the heap
  *
