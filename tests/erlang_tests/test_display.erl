a05696385660b71a4cf2b3e0f319b59c8faf6be5
Davide Bettio <davide@uninstall.it>
2018-11-04 02:01:01 +0100

    tests: simplify test_display.erl
    This test doesn't allow to find regressions right now, simplify it.

--- tests/erlang_tests/test_display.erl
+++ tests/erlang_tests/test_display.erl
@@ -2,5 +2,5 @@
 -export([start/0]).
 
 start() ->
-    erlang:display([{a, 1}, {b, 2}, false, [1, 2], [], "hello world", <<"this is a binary">>, self(), make_ref()]),
+    erlang:display([1, 2, 3, 4]),
     0.
