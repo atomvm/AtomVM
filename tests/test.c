84d1fe1066849915503ba1f71030302d6db04b40
Davide Bettio <davide@uninstall.it>
2018-11-08 01:36:01 +0100

    tests: add test_list_to_existing_atom.erl
    Add test for erlang:list_to_existing_atom/1 function.

--- tests/test.c
+++ tests/test.c
@@ -122,6 +122,7 @@ struct Test tests[] =
     {"test_make_tuple.beam", 4},
     {"test_tl.beam", 5},
     {"test_list_to_atom.beam", 9},
+    {"test_list_to_existing_atom.beam", 9},
     {"test_binary_to_atom.beam", 9},
     {"test_atom_to_list.beam", 1},
     {"test_display.beam", 0},
