42efa26d58e634f24f9783547a420530feefcc1f
Davide Bettio <davide@uninstall.it>
2018-11-08 01:29:27 +0100

    NIFs: add erlang:list_to_existing_atom/1
    Implement function that returns an existing atom from given list string.

--- src/libAtomVM/nifs.c
+++ src/libAtomVM/nifs.c
@@ -19,6 +19,7 @@
 
 #include "nifs.h"
 
+#include "atomshashtable.h"
 #include "context.h"
 #include "interop.h"
 #include "mailbox.h"
@@ -54,6 +55,7 @@ static term nif_erlang_make_ref_0(Context *ctx, int argc, term argv[]);
 static term nif_erlang_make_tuple_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_insert_element_3(Context *ctx, int argc, term argv[]);
 static term nif_erlang_list_to_atom_1(Context *ctx, int argc, term argv[]);
+static term nif_erlang_list_to_existing_atom_1(Context *ctx, int argc, term argv[]);
 static term nif_erlang_open_port_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_register_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_send_2(Context *ctx, int argc, term argv[]);
@@ -105,6 +107,12 @@ static const struct Nif list_to_atom_nif =
 {
     .base.type = NIFFunctionType,
     .nif_ptr = nif_erlang_list_to_atom_1
+};
+
+static const struct Nif list_to_existing_atom_nif =
+{
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_erlang_list_to_existing_atom_1
 };
 
 static const struct Nif open_port_nif =
@@ -737,6 +745,40 @@ term nif_erlang_list_to_atom_1(Context *ctx, int argc, term argv[])
 
     int global_atom_index = globalcontext_insert_atom(ctx->global, atom);
     return term_from_atom_index(global_atom_index);
+}
+
+term nif_erlang_list_to_existing_atom_1(Context *ctx, int argc, term argv[])
+{
+    if (argc != 1) {
+        fprintf(stderr, "list_to_existing_atom: wrong args count\n");
+        abort();
+    }
+
+    char *atom_string = interop_list_to_string(argv[0]);
+    if (IS_NULL_PTR(atom_string)) {
+        fprintf(stderr, "Failed to alloc temporary string\n");
+        abort();
+    }
+    int atom_string_len = strlen(atom_string);
+    if (UNLIKELY(atom_string_len > 255)) {
+        fprintf(stderr, "Too long atom.\n");
+        free(atom_string);
+        abort();
+    }
+
+    AtomString *atom = malloc(atom_string_len + 1);
+    ((uint8_t *) atom)[0] = atom_string_len;
+    memcpy(((char *) atom) + 1, atom_string, atom_string_len);
+
+    if (atomshashtable_has_key(ctx->global->atoms_table, atom)) {
+        int global_atom_index = globalcontext_insert_atom(ctx->global, atom);
+        return term_from_atom_index(global_atom_index);
+
+    } else {
+        free(atom);
+        //TODO: error here
+        return term_nil();
+    }
 }
 
 static term nif_erlang_atom_to_list_1(Context *ctx, int argc, term argv[])
