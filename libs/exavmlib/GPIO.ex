46c9d4f3d5775617299b0385097eba71db0e24cc
Davide Bettio <davide@uninstall.it>
2018-11-22 14:53:35 +0100

    Add ExAVMLib and Elixir GPIO module
    Add directory for AtomVM specific elixir modules and Elixir GPIO module.

---
+++ libs/exavmlib/GPIO.ex
@@ -0,0 +1,21 @@
+defmodule GPIO do
+  def open() do
+    :erlang.open_port({:spawn, "gpio"}, [])
+  end
+
+  def set_direction(gpio, gpio_num, direction) do
+    send gpio, {self(), :set_direction, gpio_num, direction}
+    receive do
+      ret ->
+        ret
+    end
+  end
+
+  def set_level(gpio, gpio_num, level) do
+    send gpio, {self(), :set_level, gpio_num, level}
+    receive do
+      ret ->
+        ret
+    end
+  end
+end
