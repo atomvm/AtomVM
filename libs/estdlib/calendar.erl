81330f4a15b46003e0bdf7fab1cdb97d05500ea4
Davide Bettio <davide@uninstall.it>
2018-11-22 01:01:13 +0100

    estdlib: calendar: export date_to_gregorian_days function
    Export date_to_gregorian_days function and other minor changes.

--- libs/estdlib/calendar.erl
+++ libs/estdlib/calendar.erl
@@ -1,20 +1,21 @@
 -module(calendar).
 
--export([day_of_the_week/1, day_of_the_week/3]).
+-export([date_to_gregorian_days/1, date_to_gregorian_days/3, day_of_the_week/1, day_of_the_week/3]).
 
-day_of_the_week({Y, M, D}) ->
-    day_of_the_week(Y, M, D).
+date_to_gregorian_days({Y, M, D}) ->
+    date_to_gregorian_days(Y, M, D).
 
-day_of_the_week(Y, M, D) ->
-    (unix_days(Y, M, D) + 4) rem 7.
-
-unix_days(Year, M, D) when Year >= 0 ->
+date_to_gregorian_days(Year, M, D) when M =< 12 andalso D =< 31 ->
     Y =
         if
             M =< 2 -> Year - 1;
             true -> Year
         end,
-    Era = Y div 400,
+    Era =
+        if
+            Y >= 0 -> Y div 400;
+            true -> Y - 399 div 400
+        end,
     YoE = Y - Era * 400,
     MO =
         if
@@ -23,4 +24,10 @@ unix_days(Year, M, D) when Year >= 0 ->
         end,
     DoY = (153 * (M + MO) + 2) div 5 + D - 1,
     DoE = YoE * 365 + YoE div 4 - YoE div 100 + DoY,
-    Era * 146097 + DoE - 719468.
+    Era * 146097 + DoE + 60.
+
+day_of_the_week({Y, M, D}) ->
+    day_of_the_week(Y, M, D).
+
+day_of_the_week(Y, M, D) ->
+    (date_to_gregorian_days(Y, M, D) + 6) rem 7.
