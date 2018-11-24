12b382de009844d934c9ac366f3f724185301ef6
Davide Bettio <davide@uninstall.it>
2018-11-24 12:06:39 +0100

    Add Console module to ExAVMLib
    Add simple Console module with puts function to Elixir AtomVM library.

---
+++ libs/exavmlib/Console.ex
@@ -0,0 +1,33 @@
+defmodule Console do
+
+  def puts(device \\ :stdio, item) do
+    pid =
+      case :erlang.whereis(device) do
+        :undefined ->
+          case device do
+            :stdio ->
+              new_pid = :erlang.open_port({:spawn, "console"}, [])
+              :erlang.register(:stdio, new_pid)
+              new_pid
+
+            _ ->
+              :error
+          end
+
+        pid ->
+          pid
+      end
+
+    write(pid, item)
+  end
+
+  defp write(console, string) do
+    send console, {self(), string}
+
+    receive do
+      return_status ->
+        return_status
+    end
+  end
+
+end
