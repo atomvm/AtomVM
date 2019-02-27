228da2c1460c33a740c16cb163fc6fb921cf0516
Davide Bettio <davide@uninstall.it>
2019-02-27 14:54:16 +0100

    Move opcodes away from opcodesswitch.h to opcodes.h
    opcodesswitch.h is getting huge, so it is a good idea to split it in
    smaller headers.

--- src/libAtomVM/opcodesswitch.h
+++ src/libAtomVM/opcodesswitch.h
@@ -27,6 +27,7 @@
 #include "utils.h"
 #include "scheduler.h"
 #include "nifs.h"
+#include "opcodes.h"
 
 #ifdef IMPL_EXECUTE_LOOP
     #include "mailbox.h"
@@ -328,85 +329,6 @@
         remaining_reductions = DEFAULT_REDUCTIONS_AMOUNT;                                         \
         JUMP_TO_ADDRESS(scheduled_context->saved_ip);                                             \
     }
-
-#define OP_LABEL 1
-#define OP_FUNC_INFO 2
-#define OP_INT_CALL_END 3
-#define OP_CALL 4
-#define OP_CALL_LAST 5
-#define OP_CALL_ONLY 6
-#define OP_CALL_EXT 7
-#define OP_CALL_EXT_LAST 8
-#define OP_BIF0 9
-#define OP_BIF1 10
-#define OP_BIF2 11
-#define OP_ALLOCATE 12
-#define OP_ALLOCATE_HEAP 13
-#define OP_ALLOCATE_ZERO 14
-#define OP_ALLOCATE_HEAP_ZERO 15
-#define OP_TEST_HEAP 16
-#define OP_KILL 17
-#define OP_DEALLOCATE 18
-#define OP_RETURN 19
-#define OP_SEND 20
-#define OP_REMOVE_MESSAGE 21
-#define OP_TIMEOUT 22
-#define OP_LOOP_REC 23
-#define OP_LOOP_REC_END 24
-#define OP_WAIT 25
-#define OP_WAIT_TIMEOUT 26
-#define OP_IS_LT 39
-#define OP_IS_GE 40
-#define OP_IS_EQUAL 41
-#define OP_IS_NOT_EQUAL 42
-#define OP_IS_EQ_EXACT 43
-#define OP_IS_NOT_EQ_EXACT 44
-#define OP_IS_INTEGER 45
-#define OP_IS_NUMBER 47
-#define OP_IS_ATOM 48
-#define OP_IS_PID 49
-#define OP_IS_REFERENCE 50
-#define OP_IS_PORT 51
-#define OP_IS_NIL 52
-#define OP_IS_BINARY 53
-#define OP_IS_LIST 55
-#define OP_IS_NONEMPTY_LIST 56
-#define OP_IS_TUPLE 57
-#define OP_TEST_ARITY 58
-#define OP_SELECT_VAL 59
-#define OP_SELECT_TUPLE_ARITY 60
-#define OP_JUMP 61
-#define OP_MOVE 64
-#define OP_GET_LIST 65
-#define OP_GET_TUPLE_ELEMENT 66
-#define OP_SET_TUPLE_ELEMENT 67
-#define OP_PUT_LIST 69
-#define OP_PUT_TUPLE 70
-#define OP_PUT 71
-#define OP_BADMATCH 72
-#define OP_IF_END 73
-#define OP_CASE_END 74
-#define OP_CALL_FUN 75
-#define OP_IS_FUNCTION 77
-#define OP_CALL_EXT_ONLY 78
-#define OP_MAKE_FUN2 103
-#define OP_TRY 104
-#define OP_TRY_END 105
-#define OP_TRY_CASE 106
-#define OP_TRY_CASE_END 107
-#define OP_APPLY 112
-#define OP_APPLY_LAST 113
-#define OP_IS_BOOLEAN 114
-#define OP_IS_FUNCTION2 115
-#define OP_GC_BIF1 124
-#define OP_GC_BIF2 125
-#define OP_TRIM 136
-#define OP_RECV_MARK 150
-#define OP_RECV_SET 151
-#define OP_LINE 153
-#define OP_IS_TAGGED_TUPLE 159
-#define OP_GET_HD 162
-#define OP_GET_TL 163
 
 #define INSTRUCTION_POINTER() \
     ((const void *) &code[i])
