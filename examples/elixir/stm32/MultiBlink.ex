3d2bf919297cb64a52841ba6c286503e18dc465a
Riccardo Binetti <rbino@gmx.com>
2019-03-24 15:11:01 +0100

    stm32: move MultiBlink example to single file
    This eases the build process for the examples

---
+++ examples/elixir/stm32/MultiBlink.ex
@@ -0,0 +1,29 @@
+defmodule MultiBlink do
+  def start do
+    spawn(__MODULE__, :blink, [{:d, 12}, 1000])
+    spawn(__MODULE__, :blink, [{:d, 13}, 500])
+    spawn(__MODULE__, :blink, [{:d, 14}, 1500])
+    spawn(__MODULE__, :blink, [{:d, 15}, 300])
+
+    loop()
+  end
+
+  def loop do
+    loop()
+  end
+
+  def blink(gpio, interval_ms) do
+    gpio_driver = GPIO.open()
+    GPIO.set_direction(gpio_driver, gpio, :output)
+
+    blink_loop(gpio_driver, gpio, interval_ms, 0)
+  end
+
+  def blink_loop(gpio_driver, gpio, interval_ms, level) do
+    GPIO.set_level(gpio_driver, gpio, level)
+
+    :timer.sleep(interval_ms)
+
+    blink_loop(gpio_driver, gpio, interval_ms, 1 - level)
+  end
+end
