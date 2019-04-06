8bef75b45f99e22e19727638e81059048f19cefc
Davide Bettio <davide@uninstall.it>
2019-04-06 15:54:03 +0200

    NIFs: add binary:part/3 function
    Add function that extracts a binary part.

--- src/libAtomVM/nifs.c
+++ src/libAtomVM/nifs.c
@@ -71,6 +71,7 @@ static term list_to_atom(Context *ctx, int argc, term argv[], int create_new);
 static term nif_binary_at_2(Context *ctx, int argc, term argv[]);
 static term nif_binary_first_1(Context *ctx, int argc, term argv[]);
 static term nif_binary_last_1(Context *ctx, int argc, term argv[]);
+static term nif_binary_part_3(Context *ctx, int argc, term argv[]);
 static term nif_binary_split_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_delete_element_2(Context *ctx, int argc, term argv[]);
 static term nif_erlang_atom_to_list_1(Context *ctx, int argc, term argv[]);
@@ -118,6 +119,12 @@ static const struct Nif binary_last_nif =
 {
     .base.type = NIFFunctionType,
     .nif_ptr = nif_binary_last_1
+};
+
+static const struct Nif binary_part_nif =
+{
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_binary_part_3
 };
 
 static const struct Nif binary_split_nif =
@@ -1346,6 +1353,40 @@ static term nif_binary_last_1(Context *ctx, int argc, term argv[])
     }
 
     return term_from_int11(term_binary_data(bin_term)[size - 1]);
+}
+
+static term nif_binary_part_3(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    term bin_term = argv[0];
+    term pos_term = argv[1];
+    term len_term = argv[2];
+
+    VALIDATE_VALUE(bin_term, term_is_binary);
+    VALIDATE_VALUE(pos_term, term_is_integer);
+    VALIDATE_VALUE(len_term, term_is_integer);
+
+    int bin_size = term_binary_size(bin_term);
+    int pos = term_to_int32(pos_term);
+    int len = term_to_int32(len_term);
+
+    if (len < 0) {
+        pos += len;
+        len = -len;
+    }
+
+    if (UNLIKELY((pos < 0) || (pos > bin_size) || (pos + len > bin_size))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+
+    // + 2, which is the binary header size
+    if (UNLIKELY(memory_ensure_free(ctx, term_binary_data_size_in_terms(len) + 2) != MEMORY_GC_OK)) {
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+
+    const char *bin_data = term_binary_data(argv[0]);
+    return term_from_literal_binary(bin_data + pos, len, ctx);
 }
 
 static term nif_binary_split_2(Context *ctx, int argc, term argv[])
