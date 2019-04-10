1165b036c583c67862a2eedc94166f2d481bd854
Davide Bettio <davide@uninstall.it>
2019-04-10 14:38:28 +0200

    tests: add test_*_to_binary.erl and test_binary_to_list.erl
    Add binary funtions related tests, such as test_atom_to_binary.erl,
    test_binary_to_list.erl, test_integer_to_binary.erl, and
    test_list_to_binary.erl.

---
+++ tests/erlang_tests/test_integer_to_binary.erl
@@ -0,0 +1,29 @@
+-module(test_integer_to_binary).
+-export([start/0, some_calculation/2, compare_bin/3]).
+
+start() ->
+    NewBin1 = integer_to_binary(some_calculation(20, -2)),
+    NewBin2 = integer_to_binary(some_calculation(1780, 0)),
+    compare_bin(NewBin1, <<"-1">>) + compare_bin(NewBin2, <<"89">>)
+    - compare_bin(NewBin1, <<"+1">>) * 10 - compare_bin(NewBin2, <<"88">>) * 100.
+
+some_calculation(N, A) when is_integer(N) and is_integer(A) ->
+    N div 20 + A;
+
+some_calculation(_N, _A) ->
+    -1.
+
+compare_bin(Bin1, Bin2) ->
+    compare_bin(Bin1, Bin2, byte_size(Bin1) - 1).
+
+compare_bin(_Bin1, _Bin2, -1) ->
+    1;
+
+compare_bin(Bin1, Bin2, Index) ->
+    B1 = binary:at(Bin1, Index),
+    case binary:at(Bin2, Index) of
+        B1 ->
+            compare_bin(Bin1, Bin2, Index - 1);
+        _Any ->
+            0
+    end.
---
+++ tests/erlang_tests/test_list_to_binary.erl
@@ -0,0 +1,37 @@
+-module(test_list_to_binary).
+-export([start/0, concat/2, concat2/2, compare_bin/3]).
+
+start() ->
+    Bin = concat("Hello", "world"),
+    Bin2 = concat2("", ""),
+    CompRes1 = compare_bin(Bin, <<"Hello world">>) - compare_bin(Bin, <<"HelloXworld">>),
+    CompRes1 + byte_size(Bin2) + invalid(42).
+
+concat(A, B) ->
+    list_to_binary(A ++ " " ++ B).
+
+concat2(A, B) ->
+    list_to_binary(A ++ B).
+
+invalid(A) ->
+    try list_to_binary(A) of
+        Any -> byte_size(Any)
+    catch
+        error:badarg -> 0;
+        _:_ -> 1000
+    end.
+
+compare_bin(Bin1, Bin2) ->
+    compare_bin(Bin1, Bin2, byte_size(Bin1) - 1).
+
+compare_bin(_Bin1, _Bin2, -1) ->
+    1;
+
+compare_bin(Bin1, Bin2, Index) ->
+    B1 = binary:at(Bin1, Index),
+    case binary:at(Bin2, Index) of
+        B1 ->
+            compare_bin(Bin1, Bin2, Index - 1);
+        _Any ->
+            0
+    end.
