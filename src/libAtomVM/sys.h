33e3ebaf06f86dd022c454a89198ca63ec7d4bfd
elsbiet <g@spllg.de>
2019-04-18 17:12:52 +0200

    Update sys.h
    added a comment on EventListener . listeners_list_head

--- src/libAtomVM/sys.h
+++ src/libAtomVM/sys.h
@@ -37,7 +37,7 @@
 typedef void (*event_handler_t)(void *data);
 
 typedef struct EventListener {
-    struct ListHead listeners_list_head;    // coution: never move this component behind any other
+    struct ListHead listeners_list_head;    // caution: never move this component behind any other
                                             // because scheduler relies on it to be the first component
 
     int expires;
