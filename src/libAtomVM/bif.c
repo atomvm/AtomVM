d4aa9da65546929abc340406c2e9563e4c0566c6
Davide Bettio <davide@uninstall.it>
2019-05-18 14:19:20 +0200

    bif: abort() on unimplemented code
    Comparison BIFs are not yet implemented for non integer terms, so abort
    when trying to use them.

--- src/libAtomVM/bif.c
+++ src/libAtomVM/bif.c
@@ -513,6 +513,11 @@ term bif_erlang_greater_than_2(Context *ctx, term arg1, term arg2)
     UNUSED(ctx);
 
     //TODO: fix this implementation, it needs to cover more types
+    if (UNLIKELY(!(term_is_integer(arg1) && term_is_integer(arg2)))) {
+        abort();
+    }
+
+    //TODO: fix this implementation, it needs to cover more types
     if (term_to_int32(arg1) > term_to_int32(arg2)) {
         return TRUE_ATOM;
     } else {
@@ -523,6 +528,11 @@ term bif_erlang_greater_than_2(Context *ctx, term arg1, term arg2)
 term bif_erlang_less_than_2(Context *ctx, term arg1, term arg2)
 {
     UNUSED(ctx);
+
+    //TODO: fix this implementation, it needs to cover more types
+    if (UNLIKELY(!(term_is_integer(arg1) && term_is_integer(arg2)))) {
+        abort();
+    }
 
     //TODO: fix this implementation, it needs to cover more types
     if (term_to_int32(arg1) < term_to_int32(arg2)) {
@@ -537,6 +547,11 @@ term bif_erlang_less_than_or_equal_2(Context *ctx, term arg1, term arg2)
     UNUSED(ctx);
 
     //TODO: fix this implementation, it needs to cover more types
+    if (UNLIKELY(!(term_is_integer(arg1) && term_is_integer(arg2)))) {
+        abort();
+    }
+
+    //TODO: fix this implementation, it needs to cover more types
     if (term_to_int32(arg1) <= term_to_int32(arg2)) {
         return TRUE_ATOM;
     } else {
@@ -547,6 +562,11 @@ term bif_erlang_less_than_or_equal_2(Context *ctx, term arg1, term arg2)
 term bif_erlang_greater_than_or_equal_2(Context *ctx, term arg1, term arg2)
 {
     UNUSED(ctx);
+
+    //TODO: fix this implementation, it needs to cover more types
+    if (UNLIKELY(!(term_is_integer(arg1) && term_is_integer(arg2)))) {
+        abort();
+    }
 
     //TODO: fix this implementation, it needs to cover more types
     if (term_to_int32(arg1) >= term_to_int32(arg2)) {
