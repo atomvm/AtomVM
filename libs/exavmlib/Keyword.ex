2430bcbb2734f333f7f7d0ad26e14b959302eb1c
Davide Bettio <davide@uninstall.it>
2020-02-13 14:54:19 +0100

    Elixir library: add Elixir.Keyword
    Import some Elixir.Keyword functions from
    elixir-lang/elixir/v1.10.1/lib/elixir/lib/keyword.ex .
    
    This code is under Apache License 2.0, and complete module history can be
    found here:
    https://github.com/elixir-lang/elixir/commits/v1.10.1/lib/elixir/lib/keyword.ex

---
+++ libs/exavmlib/Keyword.ex
@@ -0,0 +1,46 @@
+defmodule Keyword do
+  def fetch(keywords, key) when is_list(keywords) and is_atom(key) do
+    case :avm_lists.keyfind(key, 1, keywords) do
+      {^key, value} -> {:ok, value}
+      false -> :error
+    end
+  end
+
+  def fetch!(keywords, key) when is_list(keywords) and is_atom(key) do
+    case :lists.keyfind(key, 1, keywords) do
+      {^key, value} -> value
+      false -> raise(KeyError, key: key, term: keywords)
+    end
+  end
+
+  def get(keywords, key, default \\ nil) when is_list(keywords) and is_atom(key) do
+    case :avm_lists.keyfind(key, 1, keywords) do
+      {^key, value} -> value
+      false -> default
+    end
+  end
+
+  def get_lazy(keywords, key, fun)
+      when is_list(keywords) and is_atom(key) and is_function(fun, 0) do
+    case :avm_lists.keyfind(key, 1, keywords) do
+      {^key, value} -> value
+      false -> fun.()
+    end
+  end
+
+  def put(keywords, key, value) when is_list(keywords) and is_atom(key) do
+    [{key, value} | delete(keywords, key)]
+  end
+
+  def delete(keywords, key) when is_list(keywords) and is_atom(key) do
+    case :avm_lists.keymember(key, 1, keywords) do
+      true -> delete_key(keywords, key)
+      _ -> keywords
+    end
+  end
+
+  defp delete_key([{key, _} | tail], key), do: delete_key(tail, key)
+  defp delete_key([{_, _} = pair | tail], key), do: [pair | delete_key(tail, key)]
+  defp delete_key([], _key), do: []
+
+end
