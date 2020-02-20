b87ebb44140c373920dd0f63bcf1ac7a4e66998b
Davide Bettio <davide@uninstall.it>
2020-02-21 00:48:59 +0100

    Add millisecond support to :erlang.system_time
    60 bits are enough for millisecond timestamps for a reasonable amount of years.
    We might regret this commit in some thousands of years.

--- src/libAtomVM/nifs.c
+++ src/libAtomVM/nifs.c
@@ -951,6 +951,9 @@ term nif_erlang_system_time_1(Context *ctx, int argc, term argv[])
     if (argv[0] == second_atom) {
         return make_maybe_boxed_int64(ctx, ts.tv_sec);
 
+    } else if (argv[0] == context_make_atom(ctx, "\xB" "millisecond")) {
+        return make_maybe_boxed_int64(ctx, ((int64_t) ts.tv_sec) * 1000 + ts.tv_nsec / 1000000);
+
     } else {
         RAISE_ERROR(BADARG_ATOM);
     }
