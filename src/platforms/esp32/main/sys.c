atomvm/AtomVM

commit 02e3afa72c8375bb2ebf39383df7fea480bb3f7a

Added support for selective linkage of port drivers, including nvs, network, socket, gpio, i2c, spi, ledc, and any other port drivers in the future.  By default, all drivers are added in the future.

src/platforms/esp32/main/sys.c
@@ -21,14 +21,10 @@
 #include "esp32_sys.h"
 
 #include "avmpack.h"
+#include "component_ports.h"
 #include "defaultatoms.h"
 #include "globalcontext.h"
-#include "gpio_driver.h"
-#include "i2cdriver.h"
-#include "network.h"
 #include "scheduler.h"
-#include "spidriver.h"
-#include "uart_driver.h"
 
 #include "trace.h"
 
@@ -159,35 +155,23 @@ Module *sys_load_module(GlobalContext *global, const char *module_name)
 }
 
 // This function allows to use AtomVM as a component on ESP32 and customize it
-__attribute__((weak)) Context *sys_create_port_fallback(Context *new_ctx, const char *driver_name, term opts)
+__attribute__((weak)) Context *sys_create_port_fallback(Context *new_ctx, const char *port_name, term opts)
 {
-    UNUSED(driver_name);
+    UNUSED(new_ctx);
     UNUSED(opts);
 
-    context_destroy(new_ctx);
+    fprintf(stderr, "Failed to load port \"%s\".  Ensure the port is configured properly in the build.\n", port_name);
+
     return NULL;
 }
 
-Context *sys_create_port(GlobalContext *glb, const char *driver_name, term opts)
+
+Context *sys_create_port(GlobalContext *glb, const char *port_name, term opts)
 {
-    Context *new_ctx = context_new(glb);
-
-    if (!strcmp(driver_name, "socket")) {
-        socket_init(new_ctx, opts);
-    } else if (!strcmp(driver_name, "network")) {
-        network_init(new_ctx, opts);
-    } else if (!strcmp(driver_name, "gpio")) {
-        gpiodriver_init(new_ctx);
-    } else if (!strcmp(driver_name, "spi")) {
-        spidriver_init(new_ctx, opts);
-    } else if (!strcmp(driver_name, "i2c")) {
-        i2cdriver_init(new_ctx, opts);
-    } else if (!strcmp(driver_name, "uart")) {
-        uart_driver_init(new_ctx, opts);
-    } else {
-        return sys_create_port_fallback(new_ctx, driver_name, opts);
+    Context *new_ctx = component_ports_create_port(port_name, glb, opts);
+    if (IS_NULL_PTR(new_ctx)) {
+        return sys_create_port_fallback(new_ctx, port_name, opts);
     }
-
     return new_ctx;
 }
 

src/platforms/generic_unix/sys.c
@@ -22,10 +22,8 @@
 
 #include "avmpack.h"
 #include "defaultatoms.h"
-#include "gpio_driver.h"
 #include "iff.h"
 #include "mapped_file.h"
-#include "network.h"
 #include "scheduler.h"
 #include "utils.h"
 
@@ -186,10 +184,6 @@ Context *sys_create_port(GlobalContext *glb, const char *driver_name, term opts)
 
     if (!strcmp(driver_name, "socket")) {
         socket_init(new_ctx, opts);
-    } else if (!strcmp(driver_name, "network")) {
-        network_init(new_ctx, opts);
-    } else if (!strcmp(driver_name, "gpio")) {
-        gpiodriver_init(new_ctx);
     } else {
 #ifdef DYNLOAD_PORT_DRIVERS
         void *handle;

.gitignore
@@ -6,6 +6,8 @@ xcode/**
 src/platforms/esp32/build/**
 src/platforms/esp32/build/**/*.d
 src/platforms/esp32/components/**
+src/platforms/esp32/main/component_nifs.txt
+src/platforms/esp32/main/component_ports.txt
 !src/platforms/esp32/components/libatomvm/
 .idea/**
 .vscode/**

src/libAtomVM/CMakeLists.txt
@@ -27,8 +27,6 @@ set(HEADER_FILES
     opcodes.h
     opcodesswitch.h
     overflow_helpers.h
-    network.h
-    network_driver.h
     nifs.h
     platform_nifs.h
     port.h
@@ -61,7 +59,6 @@ set(SOURCE_FILES
     mailbox.c
     memory.c
     module.c
-    network.c
     nifs.c
     port.c
     refc_binary.c

src/libAtomVM/network.c
@@ -1,73 +1,0 @@
-/***************************************************************************
- *   Copyright 2018 by Davide Bettio <davide@uninstall.it>                 *
- *   Copyright 2018 by Fred Dushin <fred@dushin.net>                       *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License as        *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- ***************************************************************************/
-
-#include "network.h"
-#include "context.h"
-#include "network_driver.h"
-#include "port.h"
-
-#include "context.h"
-#include "globalcontext.h"
-#include "mailbox.h"
-#include "term.h"
-#include "utils.h"
-
-static const char *const start_a = "\x5" "start";
-static const char *const ifconfig_a = "\x8" "ifconfig";
-
-static void network_consume_mailbox(Context *ctx)
-{
-    Message *message = mailbox_dequeue(ctx);
-    term msg = message->message;
-
-    if (port_is_standard_port_command(msg)) {
-        port_ensure_available(ctx, 32);
-
-        term pid = term_get_tuple_element(msg, 0);
-        term ref = term_get_tuple_element(msg, 1);
-        term cmd = term_get_tuple_element(msg, 2);
-
-        if (term_is_atom(cmd) && cmd == context_make_atom(ctx, ifconfig_a)) {
-            term reply = network_driver_ifconfig(ctx);
-            port_send_reply(ctx, pid, ref, reply);
-        } else if (term_is_tuple(cmd) && term_get_tuple_arity(cmd) == 2) {
-            term cmd_name = term_get_tuple_element(cmd, 0);
-            term config = term_get_tuple_element(cmd, 1);
-            if (cmd_name == context_make_atom(ctx, start_a)) {
-                network_driver_start(ctx, pid, ref, config);
-            } else {
-                port_send_reply(ctx, pid, ref, port_create_error_tuple(ctx, BADARG_ATOM));
-            }
-        } else {
-            port_send_reply(ctx, pid, ref, port_create_error_tuple(ctx, BADARG_ATOM));
-        }
-    } else {
-        fprintf(stderr, "WARNING: Invalid port command.  Unable to send reply");
-    }
-
-    mailbox_destroy_message(message);
-}
-
-void network_init(Context *ctx, term opts)
-{
-    UNUSED(opts);
-    ctx->native_handler = network_consume_mailbox;
-    ctx->platform_data = NULL;
-}

src/platforms/esp32/main/component.mk
@@ -1,9 +1,32 @@
 
+DEFAULT_COMPONENT_NIFS := "gpio nvs ledc"
+DEFAULT_COMPONENT_PORTS := "gpio network socket uart spi i2c"
+
 COMPONENT_NIF_FILE := $(COMPONENT_PATH)/component_nifs.txt
 COMPONENT_EXTRA_CLEAN := component_nifs.h
 
-platform_nifs.o : component_nifs.h
+COMPONENT_PORT_FILE := $(COMPONENT_PATH)/component_ports.txt
+COMPONENT_EXTRA_CLEAN := component_ports.h
 
-component_nifs.h: $(COMPONENT_PATH)/component_nifs.h.in $(if $(wildcard $(COMPONENT_NIF_FILE)),$(COMPONENT_NIF_FILE),)
+
+platform_nifs.o main.o : component_nifs.h
+
+component_nifs.h: $(COMPONENT_PATH)/component_nifs.h.in $(if $(wildcard $(COMPONENT_NIF_FILE)),$(COMPONENT_NIF_FILE),default_component_nifs)
 	@echo "Generating component_nifs.h ..."
 	@python $(COMPONENT_PATH)/component_nifs.py $(COMPONENT_PATH)/component_nifs.h.in $(COMPONENT_NIF_FILE) > $@
+
+.PHONY: default_component_nifs
+default_component_nifs:
+	@echo "# GENERATED FILE -- EDIT TO ADD OR REMOVE COMPONENT NIFS" > $(COMPONENT_PATH)/component_nifs.txt
+	@for i in $$(echo $(DEFAULT_COMPONENT_NIFS)); do echo "$${i}_nif" >> $(COMPONENT_PATH)/component_nifs.txt; done
+
+sys.o main.o : component_ports.h
+
+component_ports.h: $(COMPONENT_PATH)/component_ports.h.in $(if $(wildcard $(COMPONENT_PORT_FILE)),$(COMPONENT_PORT_FILE),default_component_ports)
+	@echo "Generating component_ports.h ..."
+	@python $(COMPONENT_PATH)/component_ports.py $(COMPONENT_PATH)/component_ports.h.in $(COMPONENT_PORT_FILE) > $@
+
+.PHONY: default_component_ports
+default_component_ports:
+	@echo "# GENERATED FILE -- EDIT TO ADD OR REMOVE COMPONENT DRIVERS" > $(COMPONENT_PATH)/component_ports.txt
+	for i in $$(echo $(DEFAULT_COMPONENT_PORTS)); do echo "$${i}_driver" >> $(COMPONENT_PATH)/component_ports.txt; done

src/platforms/esp32/main/component_nifs.h.in
@@ -1,11 +1,18 @@
 // Generated file; DO NOT EDIT
 
 #include "nifs.h"
-// INCLUDES
+#include "globalcontext.h"
+
+// DECLARATIONS
+
+static inline void component_nifs_init(GlobalContext *global)
+{
+    // INIT
+}
 
 static inline const struct Nif *component_nifs_get_nif(const char *nifname)
 {
     const struct Nif *nif = NULL;
-    // COMPONENTS
-    return nif;
+    // GET_NIF
+    return NULL;
 }

src/platforms/esp32/main/component_nifs.py
@@ -24,14 +24,30 @@ def load(path) :
         return f.read()
 
 def insert(component_nif, component_nifs_txt) :
-    includes_pos = component_nifs_txt.find("// INCLUDES")
-    buf = component_nifs_txt[:includes_pos]
-    buf += "#include <%s.h>\n" % component_nif
-    components_pos = component_nifs_txt.find("// COMPONENTS")
-    buf += component_nifs_txt[includes_pos:components_pos]
+
+    prev_pos = component_nifs_txt.find("// DECLARATIONS")
+    buf = component_nifs_txt[:prev_pos]
+    buf += "void %s_init(GlobalContext *global);\n" % component_nif
+    buf += "const struct Nif *%s_get_nif(const char *nifname);\n" % component_nif
+
+    next_pos = component_nifs_txt.find("// INIT")
+    buf += component_nifs_txt[prev_pos:next_pos]
+    buf += "%s_init(global);\n    " % component_nif
+    prev_pos = next_pos
+
+    next_pos = component_nifs_txt.find("// GET_NIF")
+    buf += component_nifs_txt[prev_pos:next_pos]
     buf += "if ((nif = %s_get_nif(nifname)) != NULL) { return nif; }\n    " % component_nif
-    buf += component_nifs_txt[components_pos:]
+    prev_pos = next_pos
+
+    buf += component_nifs_txt[prev_pos:]
     return buf
+
+def strip_line(line) :
+    idx = line.find('#')
+    if idx != -1 :
+        line = line[:idx]
+    return line.strip()
 
 def main(argv):
     if len(argv) != 3 :
@@ -42,7 +58,7 @@ def main(argv):
         component_nifs_txt = load(argv[2]) if os.path.exists(argv[2]) else ""
         work = component_nifs_in
         for component_nif in component_nifs_txt.splitlines() :
-            component_nif = component_nif.strip()
+            component_nif = strip_line(component_nif)
             if len(component_nif) > 0 :
                 work = insert(component_nif, work)
         print(work)

commit 82e53fcefa2b0f6c8bc259d2f845e3b49fd4ab77

Fix sys_create_port_fallback
02e3afa7 (Added support for selective linkage of port drivers)
introduced a regression in it, make it working again.
sys_create_port_fallback now must return a new Context, so port drivers
init functions that are called from it, should be changed accordingly.

src/platforms/esp32/main/sys.c
@@ -161,22 +161,21 @@ Module *sys_load_module(GlobalContext *global, const char *module_name)
 }
 
 // This function allows to use AtomVM as a component on ESP32 and customize it
-__attribute__((weak)) Context *sys_create_port_fallback(Context *new_ctx, const char *port_name, term opts)
+__attribute__((weak)) Context *sys_create_port_fallback(const char *driver_name, GlobalContext *global, term opts)
 {
-    UNUSED(new_ctx);
+    UNUSED(global);
     UNUSED(opts);
 
-    fprintf(stderr, "Failed to load port \"%s\".  Ensure the port is configured properly in the build.\n", port_name);
+    fprintf(stderr, "Failed to load port \"%s\".  Ensure the port is configured properly in the build.\n", driver_name);
 
     return NULL;
 }
-
 
 Context *sys_create_port(GlobalContext *glb, const char *port_name, term opts)
 {
     Context *new_ctx = component_ports_create_port(port_name, glb, opts);
     if (IS_NULL_PTR(new_ctx)) {
-        return sys_create_port_fallback(new_ctx, port_name, opts);
+        return sys_create_port_fallback(port_name, glb, opts);
     }
     return new_ctx;
 }