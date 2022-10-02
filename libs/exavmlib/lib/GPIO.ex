ca9ccade542b045cbfa7290756e71bc85381bc64
Winford <dwinford@pm.me>
2022-10-01 19:53:20 -0700

    Update elixir GPIO driver
    Adds elixir functions for new features that have been added to the
    esp32 gpio_driver.c since this driver was last updated, including
    setting pull dirtection of internal pin resistors, hold function
    for both regular opperation and deep sleep modes.
    
    Closes Issue #351
    
    Signed-off-by: Winford <dwinford@pm.me>

--- libs/exavmlib/lib/GPIO.ex
+++ libs/exavmlib/lib/GPIO.ex
@@ -19,71 +19,81 @@
 #
 
 defmodule GPIO do
+  @compile {:no_warn_undefined, [AVMPort]}
+
+  @spec open() :: pid()
   def open() do
     case :erlang.whereis(:gpio) do
       :undefined ->
-        :erlang.open_port({:spawn, "gpio"}, [])
+        AVMPort.open({:spawn, "gpio"}, [])
 
       pid ->
         pid
     end
   end
 
+  @spec read(pid(), gpio_pin()) :: :low | :high
   def read(gpio, gpio_num) do
-    send(gpio, {self(), :read, gpio_num})
-
-    receive do
-      ret ->
-        ret
-    end
+    AVMPort.call(gpio, {:read, gpio_num})
   end
 
+  @spec set_direction(pid(), gpio_pin(), direction()) :: :ok | :error
   def set_direction(gpio, gpio_num, direction) do
-    send(gpio, {self(), :set_direction, gpio_num, direction})
-
-    receive do
-      ret ->
-        ret
-    end
+    AVMPort.call(gpio, {:set_direction, gpio_num, direction})
   end
 
+  @spec set_level(pid(), gpio_pin(), digital_state()) :: :ok | :error
   def set_level(gpio, gpio_num, level) do
-    send(gpio, {self(), :set_level, gpio_num, level})
-
-    receive do
-      ret ->
-        ret
-    end
+    AVMPort.call(gpio, {:set_level, gpio_num, level})
   end
 
+  @spec set_int(pid(), non_neg_integer(), atom()) :: :ok | :error
   def set_int(gpio, gpio_num, trigger) do
-    send(gpio, {self(), :set_int, gpio_num, trigger})
-
-    receive do
-      ret -> ret
-    end
+    AVMPort.call(gpio, {:set_int, gpio_num, trigger})
   end
 
+  @spec remove_int(pid(), non_neg_integer()) :: :ok | :error
   def remove_int(gpio, gpio_num) do
-    send(gpio, {self(), :remove_int, gpio_num})
-
-    receive do
-      ret -> ret
-    end
+    AVMPort.call(gpio, {:remove_int, gpio_num})
   end
 
-  def set_pin_mode(_gpio_num, _mode),
-    do: throw(:nif_error)
-
-  def digital_write(_gpio_num, _level),
-    do: throw(:nif_error)
-
-  def digital_read(_gpio_num),
-    do: throw(:nif_error)
-
+  @spec attach_interrupt(non_neg_integer(), atom()) :: :ok | :error
   def attach_interrupt(gpio_num, mode),
     do: set_int(open(), gpio_num, mode)
 
+  @spec detach_interrupt(non_neg_integer()) :: :ok | :error
   def detach_interrupt(gpio_num),
-    do: remove_int(open(), gpio_num)
+    do: remove_int(:erlang.whereis(:gpio), gpio_num)
+
+  @spec set_pin_mode(non_neg_integer(), atom()) :: :ok | :error
+  def set_pin_mode(_gpio_num, _mode),
+    do: throw(:nif_error)
+
+  @spec digital_write(non_neg_integer(), non_neg_integer() | atom()) :: :ok | :error
+  def digital_write(_gpio_num, _level),
+    do: throw(:nif_error)
+
+  @spec digital_read(non_neg_integer()) :: :high | :low
+  def digital_read(_gpio_num),
+    do: throw(:nif_error)
+
+  @spec set_pin_pull(non_neg_integer(), atom()) :: :ok | :error
+  def set_pin_pull(_gpio_num, _pull),
+    do: throw(:nif_error)
+
+  @spec hold_en() :: :ok | :error
+  def hold_en(),
+    do: throw(:nif_error)
+
+  @spec hold_dis() :: :ok | :error
+  def hold_dis(),
+    do: throw(:nif_error)
+
+  @spec deep_sleep_hold_en() :: :ok
+  def deep_sleep_hold_en(),
+    do: throw(:nif_error)
+
+  @spec deep_sleep_hold_dis() :: :ok
+  def deep_sleep_hold_dis(),
+    do: throw(:nif_error)
 end
