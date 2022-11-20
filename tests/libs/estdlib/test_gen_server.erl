3369657da77442a29e303aa337a8ef94c1ee4e18
Paul Guyot <pguyot@kallisys.net>
2022-11-20 09:50:44 +0100

    Add gen_server timeout
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- tests/libs/estdlib/test_gen_server.erl
+++ tests/libs/estdlib/test_gen_server.erl
@@ -25,7 +25,9 @@
 
 -record(state, {
     num_casts = 0,
-    num_infos = 0
+    num_infos = 0,
+    num_timeouts = 0,
+    info_timeout = none
 }).
 
 test() ->
@@ -36,6 +38,9 @@ test() ->
     ok = test_init_exception(),
     ok = test_late_reply(),
     ok = test_concurrent_clients(),
+    ok = test_timeout_call(),
+    ok = test_timeout_cast(),
+    ok = test_timeout_info(),
     ok.
 
 test_call() ->
@@ -158,6 +163,54 @@ wait_for(P) ->
         P -> ok
     end.
 
+test_timeout_call() ->
+    {ok, Pid} = gen_server:start(?MODULE, [], []),
+    0 = gen_server:call(Pid, get_num_timeouts),
+    ok = gen_server:call(Pid, {call_reply_timeout, 10}),
+    timer:sleep(11),
+    1 = gen_server:call(Pid, get_num_timeouts),
+    ok = gen_server:call(Pid, {call_reply_timeout, infinity}),
+    timer:sleep(11),
+    1 = gen_server:call(Pid, get_num_timeouts),
+    try
+        gen_server:call(Pid, {call_noreply_timeout, 10}, 100)
+    catch
+        _:_ -> ok
+    end,
+    2 = gen_server:call(Pid, get_num_timeouts),
+    gen_server:stop(Pid),
+    ok.
+
+test_timeout_cast() ->
+    {ok, Pid} = gen_server:start(?MODULE, [], []),
+    0 = gen_server:call(Pid, get_num_timeouts),
+    ok = gen_server:cast(Pid, {cast_timeout, 10}),
+    timer:sleep(11),
+    1 = gen_server:call(Pid, get_num_timeouts),
+    ok = gen_server:cast(Pid, {cast_timeout, infinity}),
+    timer:sleep(11),
+    1 = gen_server:call(Pid, get_num_timeouts),
+    gen_server:stop(Pid),
+    ok.
+
+test_timeout_info() ->
+    {ok, Pid} = gen_server:start(?MODULE, [], []),
+    0 = gen_server:call(Pid, get_num_timeouts),
+    ok = gen_server:cast(Pid, {set_info_timeout, 10}),
+    timer:sleep(11),
+    0 = gen_server:call(Pid, get_num_timeouts),
+    Pid ! ping,
+    timer:sleep(11),
+    N = gen_server:call(Pid, get_num_timeouts),
+    true = N > 0,
+    Pid ! ping,
+    % timeout message itself is repeated as it is a handle_info message.
+    timer:sleep(30),
+    M = gen_server:call(Pid, get_num_timeouts),
+    true = M > N + 1,
+    gen_server:stop(Pid),
+    ok.
+
 %%
 %% callbacks
 %%
@@ -186,19 +239,48 @@ handle_call(get_num_casts, From, #state{num_casts = NumCasts} = State) ->
     {noreply, State#state{num_casts = 0}};
 handle_call(get_num_infos, From, #state{num_infos = NumInfos} = State) ->
     gen_server:reply(From, NumInfos),
-    {noreply, State#state{num_infos = 0}}.
+    {noreply, State#state{num_infos = 0}};
+handle_call(get_num_timeouts, _From, State) ->
+    {reply, State#state.num_timeouts, State};
+handle_call({call_reply_timeout, Timeout}, _From, State) ->
+    {reply, ok, State, Timeout};
+handle_call({call_noreply_timeout, Timeout}, _From, State) ->
+    {noreply, State, Timeout}.
 
 handle_cast(crash, _State) ->
     throw(test_crash);
 handle_cast(ping, #state{num_casts = NumCasts} = State) ->
     {noreply, State#state{num_casts = NumCasts + 1}};
+handle_cast({cast_timeout, Timeout}, State) ->
+    {noreply, State, Timeout};
+handle_cast({set_info_timeout, Timeout}, State) ->
+    {noreply, State#state{info_timeout = Timeout}};
 handle_cast(_Request, State) ->
     {noreply, State}.
 
-handle_info(ping, #state{num_infos = NumInfos} = State) ->
-    {noreply, State#state{num_infos = NumInfos + 1}};
-handle_info(_Info, State) ->
-    {noreply, State}.
+handle_info(ping, #state{num_infos = NumInfos, info_timeout = InfoTimeout} = State) ->
+    NewState = State#state{num_infos = NumInfos + 1},
+    case InfoTimeout of
+        none ->
+            {noreply, NewState};
+        Other ->
+            {noreply, NewState, Other}
+    end;
+handle_info(timeout, #state{num_timeouts = NumTimeouts, info_timeout = InfoTimeout} = State) ->
+    NewState = State#state{num_timeouts = NumTimeouts + 1},
+    case InfoTimeout of
+        none ->
+            {noreply, NewState};
+        Other ->
+            {noreply, NewState, Other}
+    end;
+handle_info(_Info, #state{info_timeout = InfoTimeout} = State) ->
+    case InfoTimeout of
+        none ->
+            {noreply, State};
+        Other ->
+            {noreply, State, Other}
+    end.
 
 terminate(_Reason, _State) ->
     ok.
