61aaf87435dca1d66512aa03b69b4a5212f2fdd7
Fred Dushin <fred@dushin.net>
2023-03-04 10:56:20 -0500

    Add support for bs_(get|skip)_utf(8|16|32)
    Signed-off-by: Fred Dushin <fred@dushin.net>

--- src/libAtomVM/bitstring.c
+++ src/libAtomVM/bitstring.c
@@ -82,10 +82,31 @@ bool bitstring_insert_any_integer(uint8_t *dst, avm_int_t offset, avm_int64_t va
     return true;
 }
 
+static bool is_invalid_codepoint(int32_t v)
+{
+    return (v < 0) || (v > 0x10FFFF) || (v >= 0xD800 && v <= 0xDFFF);
+}
+
+//
+// UTF-8 encoding
+// https://en.wikipedia.org/wiki/UTF-8
+// +----------+----------+----------+----------+----------+
+// | code pt  |  buf[0]  |  buf[1]  |  buf[2]  |  buf[3]  |
+// +----------+----------+----------+----------+----------+
+// | U+0000   | 0xxxxxxx |          |          |          |
+// +----------+----------+----------+----------+----------+
+// | U+0080   | 110xxxxx | 10xxxxxx |          |          |
+// +----------+----------+----------+----------+----------+
+// | U+0800   | 1110xxxx | 10xxxxxx | 10xxxxxx |          |
+// +----------+----------+----------+----------+----------+
+// | U+10000  | 11110xxx | 10xxxxxx | 10xxxxxx | 10xxxxxx |
+// +----------+----------+----------+----------+----------+
+//
+
 bool bitstring_utf8_encode(avm_int_t c, uint8_t *buf, size_t *out_size)
 {
     size_t sz = 0;
-    if (c < 0 || c > 0x10FFFF) {
+    if (is_invalid_codepoint(c)) {
         return false;
     }
     if (c < 0x80) {
@@ -119,10 +140,67 @@ bool bitstring_utf8_encode(avm_int_t c, uint8_t *buf, size_t *out_size)
     return true;
 }
 
+bool bitstring_utf8_decode(const uint8_t *buf, size_t len, int32_t *c, size_t *out_size)
+{
+    if (len == 0) {
+        return false;
+    } else if (len >= 4 && (buf[0] & 0xF8) == 0xF0 && ((buf[1] & 0xC0) == 0x80) && ((buf[2] & 0xC0) == 0x80) && ((buf[3] & 0xC0) == 0x80)) {
+        int32_t v = 0;
+        v |= (buf[0] & 0x07) << 18;
+        v |= (buf[1] & 0x3F) << 12;
+        v |= (buf[2] & 0x3F) << 6;
+        v |= (buf[3] & 0x3F);
+        // overlong encoding or invalid codepoint
+        if (v <= 0x10000 || v > 0x10FFFF) {
+            return false;
+        }
+        *c = v;
+        *out_size = 4;
+        return true;
+    } else if (len >= 3 && (buf[0] & 0xF0) == 0xE0 && ((buf[1] & 0xC0) == 0x80) && ((buf[2] & 0xC0) == 0x80)) {
+        int32_t v = 0;
+        v |= (buf[0] & 0x0F) << 12;
+        v |= (buf[1] & 0x3F) << 6;
+        v |= (buf[2] & 0x3F);
+        // overlong encoding or surrogate
+        if (v < 0x800 || (v >= 0xD800 && v <= 0xDFFF)) {
+            return false;
+        }
+        *c = v;
+        *out_size = 3;
+        return true;
+    } else if (len >= 2 && (buf[0] & 0xE0) == 0xC0 && ((buf[1] & 0xC0) == 0x80)) {
+        int32_t v = 0;
+        v |= (buf[0] & 0x1F) << 6;
+        v |= (buf[1] & 0x3F);
+        // overlong encoding
+        if (v < 0x80) {
+            return false;
+        }
+        *c = v;
+        *out_size = 2;
+        return true;
+    } else if ((*buf & 0x80) == 0) {
+        int32_t v = 0;
+        v |= (buf[0] & 0x7F);
+        *c = v;
+        *out_size = 1;
+        return true;
+    }
+
+    return false;
+}
+
+// UTF-16 encoding, when U in U+010000 to U+10FFFF:
+//
+//  U' = yyyyyyyyyyxxxxxxxxxx  // U - 0x10000
+//  W1 = 110110yyyyyyyyyy      // 0xD800 + yyyyyyyyyy
+//  W2 = 110111xxxxxxxxxx      // 0xDC00 + xxxxxxxxxx
+
 bool bitstring_utf16_encode(avm_int_t c, uint8_t *buf, enum BitstringFlags bs_flags, size_t *out_size)
 {
     size_t sz = 0;
-    if (c < 0 || c > 0x10FFFF) {
+    if (is_invalid_codepoint(c)) {
         return false;
     }
     if (c < 0x10000) {
@@ -158,10 +236,66 @@ bool bitstring_utf16_encode(avm_int_t c, uint8_t *buf, enum BitstringFlags bs_fl
     return true;
 }
 
+bool bitstring_utf16_decode(const uint8_t *buf, size_t len, int32_t *c, size_t *out_size, enum BitstringFlags bs_flags)
+{
+    if (len == 0) {
+        return false;
+    } else if (bs_flags & LittleEndianIntegerMask) {
+        if (len >= 4 && ((buf[1] & 0xFC) == 0xD8) && ((buf[3] & 0xFC) == 0xDC)) {
+            int32_t v = 0;
+            v |= (buf[1] & 0x03) << 18;
+            v |= (buf[0] & 0xFF) << 10;
+            v |= (buf[3] & 0x03) << 8;
+            v |= (buf[2] & 0xFF);
+            v += 0x10000;
+            if (is_invalid_codepoint(v)) {
+                return false;
+            }
+            *c = v;
+            *out_size = 4;
+            return true;
+        } else if (len >= 2) {
+            int32_t v = 0;
+            v = READ_16LE_UNALIGNED(buf);
+            if (is_invalid_codepoint(v)) {
+                return false;
+            }
+            *c = v;
+            *out_size = 2;
+            return true;
+        }
+    } else {
+        if (len >= 4 && ((buf[0] & 0xFC) == 0xD8) && ((buf[2] & 0xFC) == 0xDC)) {
+            int32_t v = 0;
+            v |= (buf[0] & 0x03) << 18;
+            v |= (buf[1] & 0xFF) << 10;
+            v |= (buf[2] & 0x03) << 8;
+            v |= (buf[3] & 0xFF);
+            v += 0x10000;
+            if (is_invalid_codepoint(v)) {
+                return false;
+            }
+            *c = v;
+            *out_size = 4;
+            return true;
+        } else if (len >= 2) {
+            int32_t v = 0;
+            v = READ_16_UNALIGNED(buf);
+            if (is_invalid_codepoint(v)) {
+                return false;
+            }
+            *c = v;
+            *out_size = 2;
+            return true;
+        }
+    }
+    return false;
+}
+
 bool bitstring_utf32_encode(avm_int_t c, uint8_t *buf, enum BitstringFlags bs_flags)
 {
     UNUSED(bs_flags);
-    if (c < 0 || c > 0x10FFFF) {
+    if (is_invalid_codepoint(c)) {
         return false;
     }
     if (bs_flags & LittleEndianIntegerMask) {
@@ -176,4 +310,33 @@ bool bitstring_utf32_encode(avm_int_t c, uint8_t *buf, enum BitstringFlags bs_fl
         *buf++ = c & 0xFF;
     }
     return true;
+}
+
+bool bitstring_utf32_decode(const uint8_t *buf, size_t len, int32_t *c, enum BitstringFlags bs_flags)
+{
+    if (len < 4) {
+        return false;
+    } else if (bs_flags & LittleEndianIntegerMask) {
+        int32_t v = 0;
+        v |= (buf[3] & 0xFF) << 24;
+        v |= (buf[2] & 0xFF) << 16;
+        v |= (buf[1] & 0xFF) << 8;
+        v |=  buf[0] & 0xFF;
+        if (is_invalid_codepoint(v)) {
+            return false;
+        }
+        *c = v;
+        return true;
+    } else {
+        int32_t v = 0;
+        v |= (buf[0] & 0xFF) << 24;
+        v |= (buf[1] & 0xFF) << 16;
+        v |= (buf[2] & 0xFF) << 8;
+        v |=  buf[3] & 0xFF;
+        if (is_invalid_codepoint(v)) {
+            return false;
+        }
+        *c = v;
+        return true;
+    }
 }
--- tests/erlang_tests/test_bs_utf.erl
+++ tests/erlang_tests/test_bs_utf.erl
@@ -24,8 +24,14 @@
 
 start() ->
     ok = test_put_utf8(),
+    ok = test_get_utf8(),
+    ok = test_skip_utf8(),
     ok = test_put_utf16(),
+    ok = test_get_utf16(),
+    ok = test_skip_utf16(),
     ok = test_put_utf32(),
+    ok = test_get_utf32(),
+    ok = test_skip_utf32(),
     0.
 
 test_put_utf8() ->
@@ -47,6 +53,71 @@ test_put_utf8() ->
     ok = assert_badarg(fun() -> <<XF2/utf8>> end),
     XF3 = ?MODULE:id(16#110000),
     ok = assert_badarg(fun() -> <<XF3/utf8>> end),
+    XF4 = ?MODULE:id(16#D800),
+    ok = assert_badarg(fun() -> <<XF4/utf8>> end),
+    ok.
+
+test_get_utf8() ->
+    B0 = ?MODULE:id(<<240, 144, 144, 183>>),
+    <<X0/utf8, Rest/binary>> = B0,
+    X0 = ?MODULE:id(16#10437),
+    <<"">> = Rest,
+
+    B1 = ?MODULE:id(<<0, 230, 154, 145>>),
+    <<_:8, X1/utf8>> = B1,
+    X1 = ?MODULE:id($暑),
+
+    B2 = ?MODULE:id(<<$g, $a, $r, $b, $a, $g, $e, 195, 169, $t, $r, $a, $s, $h>>),
+    <<Garbage:7/binary, X2/utf8, Trash:5/binary>> = B2,
+    <<"garbage">> = Garbage,
+    X2 = ?MODULE:id($é),
+    <<"trash">> = Trash,
+
+    B3 = ?MODULE:id(<<101>>),
+    <<X3/utf8>> = B3,
+    X3 = ?MODULE:id($e),
+
+    B4 = ?MODULE:id(<<127>>),
+    <<X4/utf8>> = B4,
+    X4 = ?MODULE:id(16#7F),
+
+    B5 = ?MODULE:id(<<194, 128>>),
+    <<X5/utf8>> = B5,
+    X5 = ?MODULE:id(16#80),
+
+    B6 = ?MODULE:id(<<223, 191>>),
+    <<X6/utf8>> = B6,
+    X6 = ?MODULE:id(16#7FF),
+
+    B7 = ?MODULE:id(<<224, 160, 128>>),
+    <<X7/utf8>> = B7,
+    X7 = ?MODULE:id(16#800),
+
+    XF1 = ?MODULE:id(<<"">>),
+    ok = assert_badmatch(fun() ->
+        <<XFM1/utf8>> = XF1,
+        XFM1
+    end),
+
+    %% improper encoding -- too short!
+    verify_illegal_utf8_encoding(<<240, 144, 144>>),
+
+    % encoding of D800-DFFF illegal
+    verify_illegal_utf8_encoding(<<2#11101101, 2#10100000, 2#10000000>>),
+
+    % encoding of overlong illegal
+    verify_illegal_utf8_encoding(<<2#11110000, 2#10000000, 2#10000001, 2#10000101>>),
+    verify_illegal_utf8_encoding(<<2#11100000, 2#10000001, 2#10000101>>),
+    verify_illegal_utf8_encoding(<<2#11000000, 2#10000101>>),
+
+    ok.
+
+test_skip_utf8() ->
+    B0 = ?MODULE:id(<<0, 240, 144, 144, 183, 1>>),
+    <<X:8, _X0/utf8, Y:8>> = B0,
+    0 = X,
+    1 = Y,
+
     ok.
 
 test_put_utf16() ->
@@ -95,6 +166,86 @@ test_put_utf16() ->
     ok = assert_badarg(fun() -> <<XF2/utf16>> end),
     XF3 = ?MODULE:id(16#110000),
     ok = assert_badarg(fun() -> <<XF3/utf16>> end),
+    XF4 = ?MODULE:id(16#D800),
+    ok = assert_badarg(fun() -> <<XF4/utf16>> end),
+    ok.
+
+test_get_utf16() ->
+    B0 = ?MODULE:id(<<216, 1, 220, 55>>),
+    <<X0/utf16>> = B0,
+    X0 = 16#10437,
+
+    B1 = ?MODULE:id(<<102, 145>>),
+    <<X1/utf16>> = B1,
+    $暑 = X1,
+
+    B2 = ?MODULE:id(<<0, 233>>),
+    <<X2/utf16>> = B2,
+    $é = X2,
+
+    B3 = ?MODULE:id(<<0, 101>>),
+    <<X3/utf16>> = B3,
+    $e = X3,
+
+    B4 = ?MODULE:id(<<0, 97>>),
+    <<X4/utf16-big>> = B4,
+    $a = X4,
+
+    B5 = ?MODULE:id(<<97, 0>>),
+    <<X5/utf16-little>> = B5,
+    $a = X5,
+
+    BugOTP17713 =
+        case erlang:system_info(machine) of
+            "BEAM" ->
+                Version = [
+                    list_to_integer(N)
+                 || N <- string:tokens(erlang:system_info(version), ".")
+                ],
+                Version < [12, 2];
+            _ ->
+                false
+        end,
+    if
+        BugOTP17713 ->
+            ok;
+        true ->
+            B6 = ?MODULE:id(<<97:16/native>>),
+            <<X6/utf16-native>> = B6,
+            $a = X6
+    end,
+
+    B7 = ?MODULE:id(<<216, 1, 220, 55>>),
+    <<X7/utf16-big>> = B7,
+    16#10437 = X7,
+
+    B8 = ?MODULE:id(<<1, 216, 55, 220>>),
+    <<X8/utf16-little>> = B8,
+    16#10437 = X8,
+
+    XF1 = ?MODULE:id(<<"">>),
+    ok = assert_badmatch(fun() ->
+        <<XFM1/utf16>> = XF1,
+        XFM1
+    end),
+
+    %% improper encoding -- too short!
+    verify_illegal_utf16_encoding(<<216, 1, 220>>),
+    %% improper encoding -- gt 0x10FFFF
+    verify_illegal_utf16_encoding(<<4, 0, 0, 0>>),
+    verify_illegal_utf16_encoding(maybe_illegal_utf16_encode(0)),
+    verify_illegal_utf16_encoding(maybe_illegal_utf16_encode(16#D800)),
+    verify_illegal_utf16_encoding(maybe_illegal_utf16_encode(16#DFFF)),
+    verify_illegal_utf16_encoding(maybe_illegal_utf16_encode(-1)),
+
+    ok.
+
+test_skip_utf16() ->
+    B0 = ?MODULE:id(<<0, 216, 1, 220, 55, 1>>),
+    <<X:8, _X0/utf16, Y:8>> = B0,
+    0 = X,
+    1 = Y,
+
     ok.
 
 test_put_utf32() ->
@@ -127,6 +278,57 @@ test_put_utf32() ->
     ok = assert_badarg(fun() -> <<XF2/utf32>> end),
     XF3 = ?MODULE:id(16#110000),
     ok = assert_badarg(fun() -> <<XF3/utf32>> end),
+    XF4 = ?MODULE:id(16#D800),
+    ok = assert_badarg(fun() -> <<XF4/utf32>> end),
+    ok.
+
+test_get_utf32() ->
+    B0 = ?MODULE:id(<<0, 1, 4, 55>>),
+    <<X0/utf32>> = B0,
+    16#10437 = X0,
+
+    B1 = ?MODULE:id(<<0, 0, 102, 145>>),
+    <<X1/utf32>> = B1,
+    $暑 = X1,
+
+    B2 = ?MODULE:id(<<0, 0, 0, 233>>),
+    <<X2/utf32>> = B2,
+    $é = X2,
+
+    B3 = ?MODULE:id(<<0, 0, 0, 101>>),
+    <<X3/utf32>> = B3,
+    $e = X3,
+
+    B4 = ?MODULE:id(<<0, 0, 0, 97>>),
+    <<X4/utf32>> = B4,
+    $a = X4,
+
+    B5 = ?MODULE:id(<<97, 0, 0, 0>>),
+    <<X4/utf32-little>> = B5,
+
+    B6 = ?MODULE:id(<<97:32/native>>),
+    <<X4/utf32-native>> = B6,
+
+    B7 = ?MODULE:id(<<0, 1, 4, 55>>),
+    <<X0/utf32-big>> = B7,
+
+    B8 = ?MODULE:id(<<55, 4, 1, 0>>),
+    <<X0/utf32-little>> = B8,
+
+    verify_illegal_utf32_encoding(<<1, 2, 3>>),
+    verify_illegal_utf32_encoding(<<255, 255, 255, 255>>),
+    verify_illegal_utf32_encoding(<<255, 255, 255, 255>>),
+    verify_illegal_utf32_encoding(<<0, 0, 16#D8, 16#00>>),
+    verify_illegal_utf32_encoding(<<0, 0, 16#D8, 16#FF>>),
+
+    ok.
+
+test_skip_utf32() ->
+    B0 = ?MODULE:id(<<0, 0, 1, 4, 55, 1>>),
+    <<X:8, _X0/utf32, Y:8>> = B0,
+    0 = X,
+    1 = Y,
+
     ok.
 
 id(I) -> I.
@@ -138,3 +340,43 @@ assert_badarg(F) ->
     catch
         error:badarg -> ok
     end.
+
+assert_badmatch(F) ->
+    try
+        R = F(),
+        {fail_no_ex, R}
+    catch
+        error:{badmatch, _} -> ok
+    end.
+
+verify_illegal_utf8_encoding(X) ->
+    ok = assert_badmatch(
+        fun() ->
+            <<XF/utf8>> = X,
+            XF
+        end
+    ).
+
+verify_illegal_utf16_encoding(X) ->
+    ok = assert_badmatch(
+        fun() ->
+            <<XF/utf16>> = X,
+            XF
+        end
+    ).
+
+verify_illegal_utf32_encoding(X) ->
+    ok = assert_badmatch(
+        fun() ->
+            <<XF/utf32>> = X,
+            XF
+        end
+    ).
+
+maybe_illegal_utf16_encode(C0) ->
+    C1 = C0 - 16#10000,
+    A = (C1 bsr 18) bor 16#D8,
+    B = (C1 bsr 10) band 16#FF,
+    C = ((C1 bsr 8) band 16#03) bor 16#DC,
+    D = (C1 bsr 0) band 16#FF,
+    <<A, B, C, D>>.
