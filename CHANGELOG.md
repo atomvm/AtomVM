24c07efb3772efa3d9fa3680520a5bf3eb3f3289
Davide Bettio <davide@uninstall.it>
2023-09-12 21:27:01 +0200

    Add support for `crypto:crypto_one_time/5` on ESP32
    Add support for `crypto:crypto_one_time/5` using mbedtls with `aes_128_cbc`,
    `aes_128_ctr`, `aes_256_cbc`, `aes_256_ctr`.
    
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Added configurable logging macros to stm32 platform
 - Added support for ULP wakeup on ESP32
 - Added heap growth strategies as a fine-tuning option to `spawn_opt/2,4`
+- Added `crypto:crypto_one_time/5` on ESP32
 
 ### Fixed
 
--- libs/estdlib/src/crypto.erl
+++ libs/estdlib/src/crypto.erl
@@ -26,6 +26,22 @@
 -type hash_algorithm() :: md5 | sha | sha224 | sha256 | sha384 | sha512.
 -type digest() :: binary().
 
+-type cipher_iv() ::
+    aes_128_cbc
+    | aes_192_cbc
+    | aes_256_cbc
+    | aes_128_cfb128
+    | aes_192_cfb128
+    | aes_256_cfb128
+    | aes_128_ctr
+    | aes_192_ctr
+    | aes_256_ctr.
+
+-type padding() :: none | pkcs_padding.
+
+-type crypto_opt() :: {encrypt, boolean()} | {padding, padding()}.
+-type crypto_opts() :: [crypto_opt()].
+
 %%-----------------------------------------------------------------------------
 %% @param   Type the hash algorithm
 %% @param   Data the data to hash
@@ -36,4 +52,25 @@
 %%-----------------------------------------------------------------------------
 -spec hash(Type :: hash_algorithm(), Data :: iolist()) -> digest().
 hash(_Type, _Data) ->
+    erlang:nif_error(undefined).
+
+%%-----------------------------------------------------------------------------
+%% @param   Cipher a supported cipher that makes use of IV
+%% @param   Key the encryption / decryption key
+%% @param   IV an initialization vector
+%% @param   Data to be crypted or encrypted
+%% @param   FlagOrOptions either just true for encryption (or false for decryption), or a proplist
+%%          for any additional option such as padding.
+%% @returns Returns crypted or encrypted data.
+%% @doc     Encrypted/decrypt data using given cipher, key, IV.
+%% @end
+%%-----------------------------------------------------------------------------
+-spec crypto_one_time(
+    Cipher :: cipher_iv(),
+    Key :: iodata(),
+    IV :: iodata(),
+    Data :: iodata(),
+    FlagOrOptions :: crypto_opts()
+) -> binary().
+crypto_one_time(Cipher, Key, IV, Data, FlagOrOptions) ->
     erlang:nif_error(undefined).
