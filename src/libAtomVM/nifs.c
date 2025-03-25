f83a7729a07e43d6829ca61070bda9709424ac2b
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2025-03-25 16:00:27 +0100

    Fix 0x2b error in `nif_prim_file_get_cwd_0` (#115)
    Fixes EXW-197
    
    The problem was caused by allocating a tuple without initializing it.
    This meant the 2 words had GCed item (containing moved marker `0x2B`) or
    garbage. When calling `memory_ensure_free_with_roots`, a shallow copy of
    that tuple (it was the root) attempted to move that garbage and was
    failing.
    
    Signed-off-by: Mateusz Front <mateusz.front@swmansion.com>

--- src/libAtomVM/nifs.c
+++ src/libAtomVM/nifs.c
@@ -5856,17 +5856,13 @@ static term nif_prim_file_get_cwd_0(Context *ctx, int argc, term argv[])
 {
     UNUSED(argc)
     UNUSED(argv)
+
     char cwd[PATH_MAX];
-    if (UNLIKELY(IS_NULL_PTR(cwd))) {
-        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
-    }
-
-    if (UNLIKELY(memory_ensure_free(ctx, TUPLE_SIZE(2)) != MEMORY_GC_OK)) {
-        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
-    }
-    term result_tuple = term_alloc_tuple(2, &ctx->heap);
-
-    if (UNLIKELY(IS_NULL_PTR(getcwd(cwd, PATH_MAX)))) {
+    if (IS_NULL_PTR(getcwd(cwd, PATH_MAX))) {
+        if (UNLIKELY(memory_ensure_free(ctx, TUPLE_SIZE(2)) != MEMORY_GC_OK)) {
+            RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+        }
+        term result_tuple = term_alloc_tuple(2, &ctx->heap);
         term reason = UNDEFINED_ATOM;
         switch (errno) {
             case EACCES:
@@ -5888,13 +5884,14 @@ static term nif_prim_file_get_cwd_0(Context *ctx, int argc, term argv[])
     }
 
     size_t cwd_length = strlen(cwd);
-    if (UNLIKELY(memory_ensure_free_with_roots(ctx, term_binary_heap_size(cwd_length), 1, &result_tuple, MEMORY_CAN_SHRINK) != MEMORY_GC_OK)) {
+    if (UNLIKELY(memory_ensure_free(ctx, TUPLE_SIZE(2) + term_binary_heap_size(cwd_length)) != MEMORY_GC_OK)) {
         RAISE_ERROR(OUT_OF_MEMORY_ATOM);
     }
-    term result = term_from_literal_binary(cwd, cwd_length, &ctx->heap, ctx->global);
+    term result_tuple = term_alloc_tuple(2, &ctx->heap);
+    term cwd_binary = term_from_literal_binary(cwd, cwd_length, &ctx->heap, ctx->global);
 
     term_put_tuple_element(result_tuple, 0, OK_ATOM);
-    term_put_tuple_element(result_tuple, 1, result);
+    term_put_tuple_element(result_tuple, 1, cwd_binary);
     return result_tuple;
 }
 
