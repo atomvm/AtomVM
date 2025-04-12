8eb19b9cfb3493e7c9e4e6a6b45fd12c970d8605
Paul Guyot <pguyot@kallisys.net>
2025-04-12 13:59:13 +0200

    Add `io:fwrite/1,2,3` and `io:format/3`
    Also add few io functions required by remote shell
    
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- libs/estdlib/src/io.erl
+++ libs/estdlib/src/io.erl
@@ -27,27 +27,136 @@
 %%-----------------------------------------------------------------------------
 -module(io).
 
--export([format/1, format/2, get_line/1, put_chars/1, put_chars/2]).
+-export([
+    requests/1,
+    format/1,
+    format/2,
+    format/3,
+    fwrite/1,
+    fwrite/2,
+    fwrite/3,
+    get_line/1,
+    put_chars/1,
+    put_chars/2,
+    scan_erl_exprs/4,
+    columns/0,
+    columns/1,
+    getopts/0,
+    getopts/1,
+    printable_range/0
+]).
+
+-export_type([device/0, format/0, standard_io/0, standard_error/0, user/0]).
+
+-type format() :: atom() | string() | binary().
+-type standard_io() :: standard_io.
+-type standard_error() :: standard_error.
+-type user() :: user.
+-type device() :: atom() | pid() | standard_io() | standard_error() | user().
+
+-type getopt() ::
+    {echo, boolean()}
+    | {binary, boolean()}
+    | {encoding, unicode}
+    | {terminal, boolean()}
+    | {stdin, boolean()}
+    | {stdout, boolean()}
+    | {stderr, boolean()}.
+
+%% @priv
+requests(Requests) ->
+    execute_request(group_leader(), {requests, Requests}).
 
 %%-----------------------------------------------------------------------------
-%% @doc     Equivalent to format(Format, []).
+%% @equiv columns(standard_io)
+%% @doc Returns the number of columns for standard I/O.
 %% @end
 %%-----------------------------------------------------------------------------
--spec format(Format :: string()) -> string().
-format(Format) when is_list(Format) ->
-    format(Format, []).
+-spec columns() -> {ok, pos_integer()} | {error, enotsup}.
+columns() ->
+    columns(standard_io).
+
+%%-----------------------------------------------------------------------------
+%% @param IODevice IO device to get columns of
+%% @doc Returns the number of columns for passed I/O device.
+%% @end
+%%-----------------------------------------------------------------------------
+-spec columns(IODevice :: device()) -> {ok, pos_integer()} | {error, enotsup}.
+columns(_IODevice) ->
+    {ok, 80}.
+
+%%-----------------------------------------------------------------------------
+%% @equiv getopts(standard_io)
+%% @doc Get all options for standard I/O
+%% @end
+%%-----------------------------------------------------------------------------
+-spec getopts() -> [getopt()] | {error, Reason :: any()}.
+getopts() ->
+    getopts(standard_io).
+
+%%-----------------------------------------------------------------------------
+%% @param IODevice IO device to get options for
+%% @doc Get all options for a given IODevice
+%% @end
+%%-----------------------------------------------------------------------------
+-spec getopts(IODevice :: device()) -> [getopt()] | {error, Reason :: any()}.
+getopts(standard_io) ->
+    [
+        {echo, true},
+        {binary, false},
+        {encoding, unicode},
+        {terminal, true},
+        {stdout, true},
+        {stderr, true},
+        {stdin, true}
+    ].
+
+%%-----------------------------------------------------------------------------
+%% @doc Returns the user-requested range of printable Unicode characters.
+%% Currently always returns `unicode'
+%% @end
+%%-----------------------------------------------------------------------------
+-spec printable_range() -> unicode | latin1.
+printable_range() ->
+    unicode.
+
+%% @equiv fwrite(Format)
+-spec format(Format :: format()) -> ok.
+format(Format) ->
+    fwrite(Format).
+
+%% @equiv fwrite(Format, [])
+-spec fwrite(Format :: format()) -> ok.
+fwrite(Format) ->
+    fwrite(Format, []).
+
+%% @equiv fwrite(Format, Data)
+-spec format(Format :: format(), Data :: [term()]) -> ok.
+format(Format, Data) ->
+    fwrite(Format, Data).
+
+%% @equiv fwrite(standard_io, Format, Data)
+-spec fwrite(Format :: format(), Data :: [term()]) -> ok.
+fwrite(Format, Data) ->
+    fwrite(standard_io, Format, Data).
+
+%% @equiv fwrite(IODevice, Format, Data)
+-spec format(IODevice :: device(), Format :: format(), Data :: [term()]) -> ok.
+format(IODevice, Format, Data) ->
+    fwrite(IODevice, Format, Data).
 
 %%-----------------------------------------------------------------------------
 %% @param   Format format string
-%% @param   Args format argument
-%% @returns string
-%% @doc     Format string and data to console.
+%% @param   Data format arguments
+%% @param   IODevice device to write to
+%% @returns ok
+%% @doc     Format string and data to IO device.
 %%          See io_lib:format/2 for information about
 %%          formatting capabilities.
 %% @end
 %%-----------------------------------------------------------------------------
--spec format(Format :: string(), Args :: list()) -> string().
-format(Format, Args) when is_list(Format) andalso is_list(Args) ->
+-spec fwrite(IODevice :: device(), Format :: string(), Args :: list()) -> string().
+fwrite(IODevice, Format, Args) when is_list(Format) andalso is_list(Args) ->
     Msg =
         try
             io_lib:format(Format, Args)
@@ -55,7 +164,26 @@ format(Format, Args) when is_list(Format) andalso is_list(Args) ->
             _:_ ->
                 io_lib:format("Bad format!  Format: ~p Args: ~p~n", [Format, Args])
         end,
-    put_chars(Msg).
+    put_chars(IODevice, Msg).
+
+%%-----------------------------------------------------------------------------
+%% @param   IODevice device to read from
+%% @param   Prompt prompt to print
+%% @param   StartLocation start location for reading
+%% @param   Options options for tokenizing
+%% @returns read tokens or an error
+%% @doc     Reads data from IODevice with a given prompt
+%% @end
+%%-----------------------------------------------------------------------------
+-spec scan_erl_exprs(IODevice :: device(), Prompt :: atom() | unicode:chardata(), any(), any()) ->
+    {ok, Tokens :: any(), EndLocation :: any()}
+    | {eof, EndLocation :: any()}
+    | eof
+    | {error, any()}.
+scan_erl_exprs(IODevice, Prompt, StartLocation, Options) when is_pid(IODevice) ->
+    execute_request(
+        IODevice, {get_until, unicode, Prompt, erl_scan, tokens, [StartLocation, Options]}
+    ).
 
 %%-----------------------------------------------------------------------------
 %% @param   Prompt prompt for user input
@@ -78,6 +206,7 @@ get_line(Prompt) ->
     end.
 
 %%-----------------------------------------------------------------------------
+%% @equiv   put_chars(standard_io, Chars)
 %% @param   Chars character(s) to write to console
 %% @returns ok
 %% @doc     Writes the given character(s) to the console.
@@ -85,26 +214,40 @@ get_line(Prompt) ->
 %%-----------------------------------------------------------------------------
 -spec put_chars(Chars :: list() | binary()) -> ok.
 put_chars(Chars) ->
+    put_chars(standard_io, Chars).
+
+%%-----------------------------------------------------------------------------
+%% @param   Device device to send characters to
+%% @param   Chars character(s) to write to device
+%% @returns ok
+%% @doc     Writes the given character(s) to the given device.
+%% @end
+%%-----------------------------------------------------------------------------
+-spec put_chars(Device :: device(), Chars :: list() | binary()) -> ok.
+put_chars(standard_io, Chars) ->
     Self = self(),
     case erlang:group_leader() of
         Self ->
             console:print(Chars);
         Leader ->
-            Ref = make_ref(),
-            Leader ! {io_request, self(), Ref, {put_chars, unicode, Chars}},
-            receive
-                {io_reply, Ref, Line} -> Line
-            end
-    end.
-
-%%-----------------------------------------------------------------------------
-%% @param   Chars character(s) to write to console
-%% @returns ok
-%% @doc     Writes the given character(s) to the console.
-%% @end
-%%-----------------------------------------------------------------------------
--spec put_chars(Device :: any(), Chars :: list() | binary()) -> ok.
+            execute_request(Leader, {put_chars, unicode, Chars})
+    end;
 put_chars(standard_error, Chars) ->
-    put_chars(Chars);
-put_chars(standard_output, Chars) ->
-    put_chars(Chars).
+    put_chars(standard_io, Chars).
+
+%% @priv
+convert_request({requests, Requests}) ->
+    {requests, [convert_request(Request) || Request <- Requests]};
+convert_request(nl) ->
+    {put_chars, unicode, "\n"};
+convert_request(Other) ->
+    Other.
+
+%% @priv
+execute_request(Device, Request) when is_pid(Device) ->
+    Converted = convert_request(Request),
+    Ref = make_ref(),
+    Device ! {io_request, self(), Ref, Converted},
+    receive
+        {io_reply, Ref, Result} -> Result
+    end.
