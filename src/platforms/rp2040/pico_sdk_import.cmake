02493a97e155839638dbe4b141c7a48a5e74993e
Paul Guyot <pguyot@kallisys.net>
2025-05-01 21:23:56 +0200

    Force git clone of Pico-SDK
    Avoid issues where PICO_SDK_PATH is accidentally set in the environment.
    Users could still force `PICO_SDK_PATH` using CMake defines.
    
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- src/platforms/rp2040/pico_sdk_import.cmake
+++ src/platforms/rp2040/pico_sdk_import.cmake
@@ -27,10 +27,11 @@
 # This can be dropped into an external project to help locate this SDK
 # It should be include()ed prior to project()
 
-if (DEFINED ENV{PICO_SDK_PATH} AND (NOT PICO_SDK_PATH))
-    set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
-    message("Using PICO_SDK_PATH from environment ('${PICO_SDK_PATH}')")
-endif ()
+# Commented to ensure fork is picked up as we need some changes that are not available upstream yet
+# if (DEFINED ENV{PICO_SDK_PATH} AND (NOT PICO_SDK_PATH))
+#     set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
+#     message("Using PICO_SDK_PATH from environment ('${PICO_SDK_PATH}')")
+# endif ()
 
 if (DEFINED ENV{PICO_SDK_FETCH_FROM_GIT} AND (NOT PICO_SDK_FETCH_FROM_GIT))
     set(PICO_SDK_FETCH_FROM_GIT $ENV{PICO_SDK_FETCH_FROM_GIT})
