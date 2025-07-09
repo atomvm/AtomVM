ad089a6d996b239f7336f90fdaa807f527f4fc55
Paul Guyot <pguyot@kallisys.net>
2025-07-09 07:07:53 +0200

    Test for bs_create_bin
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- tests/test.c
+++ tests/test.c
@@ -537,6 +537,8 @@ struct Test tests[] = {
 
     TEST_CASE(test_module_info),
 
+    TEST_CASE(test_op_bs_create_bin),
+
     // noisy tests, keep them at the end
     TEST_CASE_EXPECTED(spawn_opt_monitor_normal, 1),
     TEST_CASE_EXPECTED(spawn_opt_link_normal, 1),
