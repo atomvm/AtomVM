549c148c76c1b19714189fefffbec83007b1baa2
Davide Bettio <davide@uninstall.it>
2025-09-12 18:17:30 +0200

    NIFs: disable clang-format for b64_table
    The hand formatted version looks better.
    
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- src/libAtomVM/nifs.c
+++ src/libAtomVM/nifs.c
@@ -4772,6 +4772,7 @@ static term nif_console_print(Context *ctx, int argc, term argv[])
     return OK_ATOM;
 }
 
+// clang-format off
 static char b64_table[64] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                              'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                              'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
@@ -4779,6 +4780,7 @@ static char b64_table[64] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                              'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
                              'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
                              '8', '9', '+', '/'};
+// clang-format on
 
 // per https://tools.ietf.org/rfc/rfc4648.txt
 
