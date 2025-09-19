e8c9d79c38e88f1f951977d5be1241fb7895597f
Paul Guyot <pguyot@kallisys.net>
2025-09-19 18:39:19 +0200

    armv6m: fix result register bug
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- tests/libs/jit/jit_armv6m_tests.erl
+++ tests/libs/jit/jit_armv6m_tests.erl
@@ -1870,6 +1870,68 @@ wait_timeout_test() ->
     >>,
     ?assertEqual(dump_to_bin(Dump), Stream).
 
+%% Test OP_WAIT pattern that uses set_continuation_to_label
+wait_test() ->
+    State0 = ?BACKEND:new(?JIT_VARIANT_PIC, jit_stream_binary, jit_stream_binary:new(0)),
+
+    State1 = ?BACKEND:jump_table(State0, 5),
+    State2 = ?BACKEND:add_label(State1, 1),
+    Label = 2,
+    State3 = ?BACKEND:set_continuation_to_label(State2, Label),
+    State4 = ?BACKEND:call_primitive_last(State3, ?PRIM_SCHEDULE_WAIT_CP, [ctx, jit_state]),
+
+    Stream = ?BACKEND:stream(State4),
+    Dump = <<
+        "   0:	4b01      	ldr	r3, [pc, #4]	; (0x8)\n"
+        "   2:	b5f2      	push	{r1, r4, r5, r6, r7, lr}\n"
+        "   4:	449f      	add	pc, r3\n"
+        "   6:	46c0      	nop			; (mov r8, r8)\n"
+        "   8:	0000      	movs	r0, r0\n"
+        "   a:	0000      	movs	r0, r0\n"
+        "   c:	4b01      	ldr	r3, [pc, #4]	; (0x14)\n"
+        "   e:	b5f2      	push	{r1, r4, r5, r6, r7, lr}\n"
+        "  10:	449f      	add	pc, r3\n"
+        "  12:	46c0      	nop			; (mov r8, r8)\n"
+        "  14:	0000      	movs	r0, r0\n"
+        "  16:	0000      	movs	r0, r0\n"
+        "  18:	4b01      	ldr	r3, [pc, #4]	; (0x20)\n"
+        "  1a:	b5f2      	push	{r1, r4, r5, r6, r7, lr}\n"
+        "  1c:	449f      	add	pc, r3\n"
+        "  1e:	46c0      	nop			; (mov r8, r8)\n"
+        "  20:	0000      	movs	r0, r0\n"
+        "  22:	0000      	movs	r0, r0\n"
+        "  24:	4b01      	ldr	r3, [pc, #4]	; (0x2c)\n"
+        "  26:	b5f2      	push	{r1, r4, r5, r6, r7, lr}\n"
+        "  28:	449f      	add	pc, r3\n"
+        "  2a:	46c0      	nop			; (mov r8, r8)\n"
+        "  2c:	0000      	movs	r0, r0\n"
+        "  2e:	0000      	movs	r0, r0\n"
+        "  30:	4b01      	ldr	r3, [pc, #4]	; (0x38)\n"
+        "  32:	b5f2      	push	{r1, r4, r5, r6, r7, lr}\n"
+        "  34:	449f      	add	pc, r3\n"
+        "  36:	46c0      	nop			; (mov r8, r8)\n"
+        "  38:	0000      	movs	r0, r0\n"
+        "  3a:	0000      	movs	r0, r0\n"
+        "  3c:	4b01      	ldr	r3, [pc, #4]	; (0x44)\n"
+        "  3e:	b5f2      	push	{r1, r4, r5, r6, r7, lr}\n"
+        "  40:	449f      	add	pc, r3\n"
+        "  42:	46c0      	nop			; (mov r8, r8)\n"
+        "  44:	0000      	movs	r0, r0\n"
+        "  46:	0000      	movs	r0, r0\n"
+        "  48:	a700      	add	r7, pc, #0	; (adr r7, 0x4c)\n"
+        "  4a:	2633      	movs	r6, #51	; 0x33\n"
+        "  4c:	4276      	negs	r6, r6\n"
+        "  4e:	19f6      	adds	r6, r6, r7\n"
+        "  50:	9f00      	ldr	r7, [sp, #0]\n"
+        "  52:	607e      	str	r6, [r7, #4]\n"
+        "  54:	6f57      	ldr	r7, [r2, #116]	; 0x74\n"
+        "  56:	9e05      	ldr	r6, [sp, #20]\n"
+        "  58:	9705      	str	r7, [sp, #20]\n"
+        "  5a:	46b6      	mov	lr, r6\n"
+        "  5c:	bdf2      	pop	{r1, r4, r5, r6, r7, pc}"
+    >>,
+    ?assertEqual(dump_to_bin(Dump), Stream).
+
 %% Test return_labels_and_lines/2 function
 return_labels_and_lines_test() ->
     State0 = ?BACKEND:new(?JIT_VARIANT_PIC, jit_stream_binary, jit_stream_binary:new(0)),
@@ -3310,7 +3372,7 @@ call_func_ptr_register_exhaustion_test_() ->
                             "  10:	4630      	mov	r0, r6\n"
                             "  12:	4619      	mov	r1, r3\n"
                             "  14:	47a0      	blx	r4\n"
-                            "  16:	9004      	str	r0, [sp, #16]\n"
+                            "  16:	9001      	str	r0, [sp, #4]\n"
                             "  18:	bcff      	pop	{r0, r1, r2, r3, r4, r5, r6, r7}"
                         >>,
                     ?assertEqual(dump_to_bin(Dump), Stream)
