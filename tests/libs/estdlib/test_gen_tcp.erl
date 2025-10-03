a7b75ed2fb56f466c111b2026db2e12a226a7509
Winford <winford@object.stream>
2025-10-03 06:25:51 +0000

    Fix gen_tcp_socket:connect/3 with hostnames
    The otp_socket driver only works with IP addresses, so inet:getaddr/2 is needed to ensure hostnames
    are resolved before passing them to socket:connect/2.
    
    Add tests for `gen_tcp:connect/3` to ensure both socket and inet backends can connect to either IP
    addresses or hostnames in both active and passive modes.
    
    Closes #1839
    
    Signed-off-by: Winford <winford@object.stream>

--- tests/libs/estdlib/test_gen_tcp.erl
+++ tests/libs/estdlib/test_gen_tcp.erl
@@ -28,6 +28,8 @@ test() ->
     ok = test_echo_server(),
     ok = test_echo_server(true),
     ok = test_listen_connect_parameters(),
+    ok = test_connect_parameters(),
+    ok = test_connect_bad_address(),
     ok = test_tcp_double_close(),
     ok.
 
@@ -346,6 +348,94 @@ test_listen_connect_parameters_server_loop(ListenMode, false = ListenActive, Soc
             end;
         Other ->
             {error, {unexpected_result, server, passive_receive, Other}}
+    end.
+
+test_connect_parameters() ->
+    IP =
+        case inet:getaddr("www.github.com", inet) of
+            {ok, IPAddress} ->
+                IPAddress;
+            Error ->
+                io:format(
+                    "Unable to resolve www.github.com, ~p; unable to complete connection tests.~n",
+                    [Error]
+                ),
+                throw(Error)
+        end,
+    Hostname = "www.atomvm.org",
+    Port = 80,
+    OptTests =
+        case get_otp_version() of
+            Version when Version =:= atomvm orelse (is_integer(Version) andalso Version >= 24) ->
+                [
+                    [{active, true}],
+                    [{active, false}],
+                    [{inet_backend, socket}, {active, true}],
+                    [{inet_backend, socket}, {active, false}]
+                ];
+            _ ->
+                [
+                    [{active, true}],
+                    [{active, false}]
+                ]
+        end,
+    test_connect_parameters(OptTests, IP, Hostname, Port, []).
+
+test_connect_parameters([], _IP, _Host, _Port, Results) ->
+    case lists:flatten(Results) of
+        [] ->
+            ok;
+        ErrorList ->
+            ErrorList
+    end;
+test_connect_parameters([Test | TestOpts], IP, Host, Port, Results) ->
+    io:format("GEN_TCP_CONNECT-TEST> IP Address=~p (www.github.com) ClientConnectOptions=~p~n", [
+        IP, Test
+    ]),
+    IpResult =
+        case gen_tcp:connect(IP, Port, Test) of
+            {ok, Soc0} ->
+                ok = gen_tcp:close(Soc0),
+                [];
+            IpErr ->
+                {error, IpErr, {ip_connect, Test}}
+        end,
+    io:format("GEN_TCP_CONNECT-TEST> Hostname=~p ClientConnectOptions=~p~n", [Host, Test]),
+    HostResult =
+        case gen_tcp:connect(Host, Port, Test) of
+            {ok, Soc1} ->
+                ok = gen_tcp:close(Soc1),
+                [];
+            HostErr ->
+                {error, HostErr, {hostname_connect, Test}}
+        end,
+    test_connect_parameters(TestOpts, IP, Host, Port, [Results, IpResult, HostResult]).
+
+test_connect_bad_address() ->
+    {error, nxdomain} = gen_tcp:connect("foo.bar.flurtleblurb", 80, []),
+    try gen_tcp:connect({532, 0, 0, 1}, 80, []) of
+        %{error, {getaddrinfo, _Er}} ->
+        %    ok;
+        {ok, Port} ->
+            gen_tcp:close(Port),
+            error(invalid_ipaddr_accepted);
+        Error ->
+            error({unexpected, Error})
+    catch
+        %& OTP
+        _:badarg ->
+            ok;
+        %& AtomVM
+        _:{badmatch, {error, {getaddrinfo, _Er}}}->
+            ok
+        %{error, {getaddrinfo, _Er}} ->
+        %    ok
+        %{getaddrinfo, _Er} ->
+        %    ok
+        %_:{error, {getaddrinfo, _Er}} ->
+        %    ok
+        %_:{getaddrinfo, _Er} ->
+        %    ok
     end.
 
 test_tcp_double_close() ->
