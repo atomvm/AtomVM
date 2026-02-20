9c18753e81a3f3ac8d41f8071807754053bfc4c5
Paul Guyot <pguyot@kallisys.net>
2026-02-20 22:12:01 +0100

    Add SPI support to RP2 platform
    Also add a sample code, `spi_flash.erl` confirmed to work on both Pico-W
    and ESP32C2, using the same high level SPI API.
    
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

---
+++ src/platforms/rp2/src/lib/spidriver.c
@@ -0,0 +1,328 @@
+/*
+ * This file is part of AtomVM.
+ *
+ * Copyright 2026 Paul Guyot <pguyot@kallisys.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+#include <stdbool.h>
+#include <string.h>
+
+#include <hardware/spi.h>
+
+#include "context.h"
+#include "defaultatoms.h"
+#include "erl_nif.h"
+#include "erl_nif_priv.h"
+#include "globalcontext.h"
+#include "interop.h"
+#include "memory.h"
+#include "nifs.h"
+#include "rp2_sys.h"
+#include "term.h"
+
+// #define ENABLE_TRACE
+#include "trace.h"
+
+#define NUM_SPI_INSTANCES 2
+
+static ErlNifResourceType *spi_resource_type;
+
+struct SPIResource
+{
+    spi_inst_t *spi_inst;
+};
+
+static term create_pair(Context *ctx, term term1, term term2)
+{
+    term ret = term_alloc_tuple(2, &ctx->heap);
+    term_put_tuple_element(ret, 0, term1);
+    term_put_tuple_element(ret, 1, term2);
+    return ret;
+}
+
+static bool get_spi_resource(Context *ctx, term resource_term, struct SPIResource **rsrc_obj)
+{
+    void *rsrc_obj_ptr;
+    if (UNLIKELY(!enif_get_resource(erl_nif_env_from_context(ctx), resource_term, spi_resource_type, &rsrc_obj_ptr))) {
+        return false;
+    }
+    *rsrc_obj = (struct SPIResource *) rsrc_obj_ptr;
+    return true;
+}
+
+static term nif_spi_init(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    VALIDATE_VALUE(argv[0], term_is_integer);
+    VALIDATE_VALUE(argv[1], term_is_integer);
+
+    int peripheral = term_to_int(argv[0]);
+    if (UNLIKELY(peripheral < 0 || peripheral >= NUM_SPI_INSTANCES)) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+
+    uint baudrate = (uint) term_to_int(argv[1]);
+    spi_inst_t *inst = (peripheral == 0) ? spi0 : spi1;
+
+    uint actual_baudrate = spi_init(inst, baudrate);
+
+    struct SPIResource *rsrc_obj = enif_alloc_resource(spi_resource_type, sizeof(struct SPIResource));
+    if (IS_NULL_PTR(rsrc_obj)) {
+        spi_deinit(inst);
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+    rsrc_obj->spi_inst = inst;
+
+    if (UNLIKELY(memory_ensure_free(ctx, TERM_BOXED_RESOURCE_SIZE) != MEMORY_GC_OK)) {
+        spi_deinit(inst);
+        enif_release_resource(rsrc_obj);
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+    term obj = enif_make_resource(erl_nif_env_from_context(ctx), rsrc_obj);
+    enif_release_resource(rsrc_obj);
+
+    size_t requested_size = TUPLE_SIZE(2) + TUPLE_SIZE(2);
+    if (UNLIKELY(memory_ensure_free_with_roots(ctx, requested_size, 1, &obj, MEMORY_CAN_SHRINK) != MEMORY_GC_OK)) {
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+
+    term inner = term_alloc_tuple(2, &ctx->heap);
+    term_put_tuple_element(inner, 0, term_from_int(actual_baudrate));
+    term_put_tuple_element(inner, 1, obj);
+
+    return create_pair(ctx, OK_ATOM, inner);
+}
+
+static term nif_spi_deinit(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    struct SPIResource *rsrc_obj;
+    if (UNLIKELY(!get_spi_resource(ctx, argv[0], &rsrc_obj))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+
+    spi_deinit(rsrc_obj->spi_inst);
+    rsrc_obj->spi_inst = NULL;
+
+    return OK_ATOM;
+}
+
+static term nif_spi_set_baudrate(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    struct SPIResource *rsrc_obj;
+    if (UNLIKELY(!get_spi_resource(ctx, argv[0], &rsrc_obj))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+    VALIDATE_VALUE(argv[1], term_is_integer);
+
+    uint baudrate = (uint) term_to_int(argv[1]);
+    uint actual = spi_set_baudrate(rsrc_obj->spi_inst, baudrate);
+
+    if (UNLIKELY(memory_ensure_free(ctx, TUPLE_SIZE(2)) != MEMORY_GC_OK)) {
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+    return create_pair(ctx, OK_ATOM, term_from_int(actual));
+}
+
+static term nif_spi_set_format(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    struct SPIResource *rsrc_obj;
+    if (UNLIKELY(!get_spi_resource(ctx, argv[0], &rsrc_obj))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+    VALIDATE_VALUE(argv[1], term_is_integer);
+    VALIDATE_VALUE(argv[2], term_is_integer);
+    VALIDATE_VALUE(argv[3], term_is_integer);
+
+    uint data_bits = (uint) term_to_int(argv[1]);
+    spi_cpol_t cpol = (spi_cpol_t) term_to_int(argv[2]);
+    spi_cpha_t cpha = (spi_cpha_t) term_to_int(argv[3]);
+
+    spi_set_format(rsrc_obj->spi_inst, data_bits, cpol, cpha, SPI_MSB_FIRST);
+
+    return OK_ATOM;
+}
+
+static term nif_spi_write_blocking(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    struct SPIResource *rsrc_obj;
+    if (UNLIKELY(!get_spi_resource(ctx, argv[0], &rsrc_obj))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+    VALIDATE_VALUE(argv[1], term_is_binary);
+
+    const uint8_t *data = (const uint8_t *) term_binary_data(argv[1]);
+    size_t len = term_binary_size(argv[1]);
+
+    int ret = spi_write_blocking(rsrc_obj->spi_inst, data, len);
+
+    return term_from_int(ret);
+}
+
+static term nif_spi_read_blocking(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    struct SPIResource *rsrc_obj;
+    if (UNLIKELY(!get_spi_resource(ctx, argv[0], &rsrc_obj))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+    VALIDATE_VALUE(argv[1], term_is_integer);
+    VALIDATE_VALUE(argv[2], term_is_integer);
+
+    uint8_t repeated_tx_data = (uint8_t) term_to_int(argv[1]);
+    avm_int_t count = term_to_int(argv[2]);
+
+    if (UNLIKELY(count < 0)) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+
+    if (UNLIKELY(memory_ensure_free_opt(ctx, TUPLE_SIZE(2) + term_binary_heap_size(count), MEMORY_NO_GC) != MEMORY_GC_OK)) {
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+    term data = term_create_uninitialized_binary(count, &ctx->heap, ctx->global);
+    uint8_t *buf = (uint8_t *) term_binary_data(data);
+
+    spi_read_blocking(rsrc_obj->spi_inst, repeated_tx_data, buf, (size_t) count);
+
+    return create_pair(ctx, OK_ATOM, data);
+}
+
+static term nif_spi_write_read_blocking(Context *ctx, int argc, term argv[])
+{
+    UNUSED(argc);
+
+    struct SPIResource *rsrc_obj;
+    if (UNLIKELY(!get_spi_resource(ctx, argv[0], &rsrc_obj))) {
+        RAISE_ERROR(BADARG_ATOM);
+    }
+    VALIDATE_VALUE(argv[1], term_is_binary);
+
+    const uint8_t *src = (const uint8_t *) term_binary_data(argv[1]);
+    size_t len = term_binary_size(argv[1]);
+
+    if (UNLIKELY(memory_ensure_free_opt(ctx, TUPLE_SIZE(2) + term_binary_heap_size(len), MEMORY_NO_GC) != MEMORY_GC_OK)) {
+        RAISE_ERROR(OUT_OF_MEMORY_ATOM);
+    }
+    term data = term_create_uninitialized_binary(len, &ctx->heap, ctx->global);
+    uint8_t *dst = (uint8_t *) term_binary_data(data);
+
+    spi_write_read_blocking(rsrc_obj->spi_inst, src, dst, len);
+
+    return create_pair(ctx, OK_ATOM, data);
+}
+
+static void spi_resource_dtor(ErlNifEnv *caller_env, void *obj)
+{
+    UNUSED(caller_env);
+    struct SPIResource *rsrc_obj = (struct SPIResource *) obj;
+    if (!IS_NULL_PTR(rsrc_obj->spi_inst)) {
+        spi_deinit(rsrc_obj->spi_inst);
+        rsrc_obj->spi_inst = NULL;
+    }
+}
+
+static const ErlNifResourceTypeInit SPIResourceTypeInit = {
+    .members = 1,
+    .dtor = spi_resource_dtor,
+};
+
+//
+// NIF structs
+//
+static const struct Nif spi_init_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_init
+};
+static const struct Nif spi_deinit_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_deinit
+};
+static const struct Nif spi_set_baudrate_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_set_baudrate
+};
+static const struct Nif spi_set_format_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_set_format
+};
+static const struct Nif spi_write_blocking_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_write_blocking
+};
+static const struct Nif spi_read_blocking_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_read_blocking
+};
+static const struct Nif spi_write_read_blocking_nif = {
+    .base.type = NIFFunctionType,
+    .nif_ptr = nif_spi_write_read_blocking
+};
+
+static void spi_nif_init(GlobalContext *global)
+{
+    ErlNifEnv env;
+    erl_nif_env_partial_init_from_globalcontext(&env, global);
+    spi_resource_type = enif_init_resource_type(&env, "spi_resource", &SPIResourceTypeInit, ERL_NIF_RT_CREATE, NULL);
+}
+
+static const struct Nif *spi_nif_get_nif(const char *nifname)
+{
+    if (strncmp("spi:", nifname, 4) != 0) {
+        return NULL;
+    }
+    const char *rest = nifname + 4;
+    if (strcmp("init/2", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_init_nif;
+    }
+    if (strcmp("deinit/1", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_deinit_nif;
+    }
+    if (strcmp("set_baudrate/2", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_set_baudrate_nif;
+    }
+    if (strcmp("set_format/4", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_set_format_nif;
+    }
+    if (strcmp("write_blocking/2", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_write_blocking_nif;
+    }
+    if (strcmp("read_blocking/3", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_read_blocking_nif;
+    }
+    if (strcmp("write_read_blocking/2", rest) == 0) {
+        TRACE("Resolved spi nif %s ...\n", nifname);
+        return &spi_write_read_blocking_nif;
+    }
+    return NULL;
+}
+
+REGISTER_NIF_COLLECTION(spi, spi_nif_init, NULL, spi_nif_get_nif)
