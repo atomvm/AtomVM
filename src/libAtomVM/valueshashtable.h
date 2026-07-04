f283ce22408ad5d9817d398bc18e1cdd0dd3651c
Peter M <petermm@gmail.com>
2026-07-04 14:06:46 +0200

    Fix memory leaks in globalcontext_destroy
    Fix modules table cleanup on globalcontext_new errors
    
    Use valueshashtable_destroy when unwinding after modules_table has been allocated so its buckets and SMP lock are released consistently with normal GlobalContext teardown.
    
    Signed-off-by: Peter M <petermm@gmail.com>

--- src/libAtomVM/valueshashtable.h
+++ src/libAtomVM/valueshashtable.h
@@ -48,6 +48,7 @@ struct ValuesHashTable
 };
 
 struct ValuesHashTable *valueshashtable_new(void);
+void valueshashtable_destroy(struct ValuesHashTable *hash_table);
 int valueshashtable_insert(struct ValuesHashTable *hash_table, uintptr_t key, uintptr_t value);
 uintptr_t valueshashtable_get_value(const struct ValuesHashTable *hash_table, uintptr_t key, uintptr_t default_value);
 int valueshashtable_has_key(const struct ValuesHashTable *hash_table, uintptr_t key);
